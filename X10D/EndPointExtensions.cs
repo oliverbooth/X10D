90e4552415861afd15f7363175fd07b6510ee44d
Oliver Booth <me@olivr.me>
2019-11-16 02:04:47 +0000

    ✨ Add EndPointExtensions
    GetHost() returns the hostname of the endpoint which will either be the
    value of DnsEndPoint.Host or IPEndPoint.Address
    
    GetPort() returns the port of the endpoint.

---
+++ X10D/EndPointExtensions.cs
@@ -0,0 +1,42 @@
+﻿namespace X10D
+{
+    #region Using Directives
+
+    using System;
+    using System.Net;
+
+    #endregion
+
+    /// <summary>
+    /// A set of extension methods for <see cref="EndPoint"/> and derived types.
+    /// </summary>
+    public static class EndPointExtensions
+    {
+        /// <summary>
+        /// Gets the endpoint hostname.
+        /// </summary>
+        /// <param name="endPoint">The endpoint whose hostname to get.</param>
+        /// <returns>Returns a <see cref="String"/> representing the hostname, which may be an IP or a DNS, or empty
+        /// string on failure.</returns>
+        public static string GetHost(this EndPoint endPoint) =>
+            endPoint switch
+            {
+                IPEndPoint ip => ip.Address.ToString(),
+                DnsEndPoint dns => dns.Host,
+                _ => String.Empty
+            };
+
+        /// <summary>
+        /// Gets the endpoint port.
+        /// </summary>
+        /// <param name="endPoint">The endpoint whose port to get.</param>
+        /// <returns>Returns an <see cref="Int32"/> representing the port, or 0 on failure.</returns>
+        public static int GetPort(this EndPoint endPoint) =>
+            endPoint switch
+            {
+                IPEndPoint ip => ip.Port,
+                DnsEndPoint dns => dns.Port,
+                _ => 0
+            };
+    }
+}
