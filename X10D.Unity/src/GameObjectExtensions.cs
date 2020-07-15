5bc71bd4264798377bca3c0af5035439837ebbf6
Oliver Booth <me@olivr.me>
2020-07-15 14:33:40 +0100

    ✨ Add X10D.Unity

---
+++ X10D.Unity/src/GameObjectExtensions.cs
@@ -0,0 +1,64 @@
+﻿namespace X10D.Unity
+{
+    using System;
+    using JetBrains.Annotations;
+    using UnityEngine;
+
+    /// <summary>
+    ///     Extension methods for <see cref="GameObject" />.
+    /// </summary>
+    public static class GameObjectExtensions
+    {
+        /// <summary>
+        ///     Rotates the <see cref="Transform" /> component on the current <see cref="GameObject" /> such that is is facing another
+        ///     <see cref="GameObject" />.
+        /// </summary>
+        /// <param name="gameObject">The current game object.</param>
+        /// <param name="other">The target.</param>
+        /// <exception cref="ArgumentNullException">
+        ///     <paramref name="gameObject" /> is null
+        ///     - or -
+        ///     <paramref name="other" /> is null.
+        /// </exception>
+        public static void LookAt([NotNull] this GameObject gameObject, [NotNull] GameObject other)
+        {
+            if (gameObject is null)
+            {
+                throw new ArgumentNullException(nameof(gameObject));
+            }
+
+            if (other is null)
+            {
+                throw new ArgumentNullException(nameof(other));
+            }
+
+            gameObject.LookAt(other.transform);
+        }
+
+        /// <summary>
+        ///     Rotates the <see cref="Transform" /> component on the current <see cref="GameObject" /> such that is is facing another
+        ///     <see cref="Transform" />.
+        /// </summary>
+        /// <param name="gameObject">The current game object.</param>
+        /// <param name="other">The target.</param>
+        /// <exception cref="ArgumentNullException">
+        ///     <paramref name="gameObject" /> is null
+        ///     - or -
+        ///     <paramref name="other" /> is null.
+        /// </exception>
+        public static void LookAt([NotNull] this GameObject gameObject, [NotNull] Transform other)
+        {
+            if (gameObject is null)
+            {
+                throw new ArgumentNullException(nameof(gameObject));
+            }
+
+            if (other is null)
+            {
+                throw new ArgumentNullException(nameof(other));
+            }
+
+            gameObject.transform.LookAt(other);
+        }
+    }
+}
---
+++ X10D.Unity/src/TransformExtensions.cs
@@ -0,0 +1,37 @@
+﻿namespace X10D.Unity
+{
+    using System;
+    using JetBrains.Annotations;
+    using UnityEngine;
+
+    /// <summary>
+    ///     Extension methods for <see cref="Transform" />.
+    /// </summary>
+    public static class TransformExtensions
+    {
+        /// <summary>
+        ///     Rotates the current <see cref="Transform" /> such that is is facing another <see cref="GameObject" />.
+        /// </summary>
+        /// <param name="transform">The current transform.</param>
+        /// <param name="other">The target.</param>
+        /// <exception cref="ArgumentNullException">
+        ///     <paramref name="transform" /> is null
+        ///     - or -
+        ///     <paramref name="other" /> is null.
+        /// </exception>
+        public static void LookAt([NotNull] this Transform transform, [NotNull] GameObject other)
+        {
+            if (transform is null)
+            {
+                throw new ArgumentNullException(nameof(transform));
+            }
+
+            if (other is null)
+            {
+                throw new ArgumentNullException(nameof(other));
+            }
+
+            transform.LookAt(other.transform);
+        }
+    }
+}
