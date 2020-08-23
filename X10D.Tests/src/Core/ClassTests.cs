5f777954cbac3b0ab89e2b944a93d4d80623e34b
rubiksmaster02 <therealrubiksmaster@gmail.com>
2020-08-22 22:37:37 -0700

    Added class extensions and tests

---
+++ X10D.Tests/src/Core/ClassTests.cs
@@ -0,0 +1,45 @@
+﻿namespace X10D.Tests.Core
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    /// <summary>
+    ///     Tests for <see cref="ClassExtensions" />.
+    /// </summary>
+    [TestClass]
+    public class ClassTests
+    {
+        /// <summary>
+        ///     Tests for <see cref="ClassExtensions.PropertiesEquals{T}" />.
+        /// </summary>
+        [TestMethod]
+        public void PropertiesEquals()
+        {
+            var a = new TempClass { FieldBool = true, FieldInt = 1, PropBool = true, PropInt = 1 };
+            var b = new TempClass { FieldBool = false, FieldInt = 0, PropBool = true, PropInt = 1 };
+            Assert.IsTrue(a.PropertiesEquals(b));
+        }
+
+        /// <summary>
+        ///     Tests for <see cref="ClassExtensions.FieldsEquals{T}" />.
+        /// </summary>
+        [TestMethod]
+        public void FieldsEquals()
+        {
+            var a = new TempClass { FieldBool = false, FieldInt = 0, PropBool = false, PropInt = 0 };
+            var b = new TempClass { FieldBool = false, FieldInt = 0, PropBool = true, PropInt = 1 };
+            Assert.IsTrue(a.FieldsEquals(b));
+        }
+
+        [Serializable]
+        private class TempClass
+        {
+            public int FieldInt;
+            public bool FieldBool;
+
+            public int PropInt { get; set; }
+
+            public bool PropBool { get; set; }
+        }
+    }
+}
---
+++ X10D/src/ClassExtensions.cs
@@ -0,0 +1,65 @@
+﻿namespace X10D
+{
+    using System.Linq;
+    using System.Reflection;
+
+    /// <summary>
+    ///     Extension methods for <see langword="struct" /> types.
+    /// </summary>
+    public static class ClassExtensions
+    {
+        /// <summary>
+        ///      Checks if all public properties of two types are equal.
+        /// </summary>
+        /// <param name="instance"> The first value being checked. </param>
+        /// <param name="comparator"> The second value being checked. </param>
+        /// <typeparam name="T"> Any type. </typeparam>
+        /// <returns> True if all properties of <param name="instance"/> equals all properties of <param name="comparator"/>. </returns>
+        public static bool PropertiesEquals<T>(this T instance, T comparator)
+            where T : class
+        {
+            return typeof(T).GetProperties().Any(e => e.GetValue(instance).Equals(e.GetValue(comparator)));
+        }
+
+        /// <summary>
+        ///      Checks if all public properties of two types are equal.
+        /// </summary>
+        /// <param name="instance"> The first value being checked. </param>
+        /// <param name="comparator"> The second value being checked. </param>
+        /// <param name="bindingFlags"> The flags that specify what fields are being checked. </param>
+        /// <typeparam name="T"> Any type. </typeparam>
+        /// <returns> True if all properties of <param name="instance"/> equals all properties of <param name="comparator"/>. </returns>
+        public static bool PropertiesEquals<T>(this T instance, T comparator, BindingFlags bindingFlags)
+            where T : class
+        {
+            return typeof(T).GetProperties(bindingFlags).Any(e => e.GetValue(instance).Equals(e.GetValue(comparator)));
+        }
+
+        /// <summary>
+        ///      Checks if all public fields of two types are equal.
+        /// </summary>
+        /// <param name="instance"> The first value being checked. </param>
+        /// <param name="comparator"> The second value being checked. </param>
+        /// <typeparam name="T"> Any type. </typeparam>
+        /// <returns> True if all fields of <param name="instance"/> equals all fields of <param name="comparator"/>. </returns>
+        public static bool FieldsEquals<T>(this T instance, T comparator)
+            where T : class
+        {
+            return typeof(T).GetFields().Any(e => e.GetValue(instance).Equals(e.GetValue(comparator)));
+        }
+
+        /// <summary>
+        ///      Checks if all public fields of two types are equal.
+        /// </summary>
+        /// <param name="instance"> The first value being checked. </param>
+        /// <param name="comparator"> The second value being checked. </param>
+        /// <param name="bindingFlags"> The flags that specify what fields are being checked. </param>
+        /// <typeparam name="T"> Any type. </typeparam>
+        /// <returns> True if all fields of <param name="instance"/> equals all fields of <param name="comparator"/>. </returns>
+        public static bool FieldsEquals<T>(this T instance, T comparator, BindingFlags bindingFlags)
+            where T : class
+        {
+            return typeof(T).GetFields(bindingFlags).Any(e => e.GetValue(instance).Equals(e.GetValue(comparator)));
+        }
+    }
+}
