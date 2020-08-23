30ea958f54d041b1be1829e95fc9b67ff0b576b7
rubiksmaster02 <therealrubiksmaster@gmail.com>
2020-08-23 09:41:01 -0700

    fixed classes code repetition

--- X10D/src/ClassExtensions.cs
+++ X10D/src/ClassExtensions.cs
@@ -9,54 +9,28 @@
     public static class ClassExtensions
     {
         /// <summary>
-        ///      Checks if all public properties of two types are equal.
-        /// </summary>
-        /// <param name="instance"> The first value being checked. </param>
-        /// <param name="comparator"> The second value being checked. </param>
-        /// <typeparam name="T"> Any type. </typeparam>
-        /// <returns> True if all properties of <param name="instance"/> equals all properties of <param name="comparator"/>. </returns>
-        public static bool PropertiesEquals<T>(this T instance, T comparator)
-            where T : class
-        {
-            return typeof(T).GetProperties().Any(e => e.GetValue(instance).Equals(e.GetValue(comparator)));
-        }
-
-        /// <summary>
-        ///      Checks if all public properties of two types are equal.
+        ///      Checks if all properties of two types are equal.
         /// </summary>
         /// <param name="instance"> The first value being checked. </param>
         /// <param name="comparator"> The second value being checked. </param>
         /// <param name="bindingFlags"> The flags that specify what fields are being checked. </param>
         /// <typeparam name="T"> Any type. </typeparam>
         /// <returns> True if all properties of <param name="instance"/> equals all properties of <param name="comparator"/>. </returns>
-        public static bool PropertiesEquals<T>(this T instance, T comparator, BindingFlags bindingFlags)
+        public static bool PropertiesEquals<T>(this T instance, T comparator, BindingFlags bindingFlags = BindingFlags.Instance | BindingFlags.Public)
             where T : class
         {
             return typeof(T).GetProperties(bindingFlags).Any(e => e.GetValue(instance).Equals(e.GetValue(comparator)));
         }
 
         /// <summary>
-        ///      Checks if all public fields of two types are equal.
-        /// </summary>
-        /// <param name="instance"> The first value being checked. </param>
-        /// <param name="comparator"> The second value being checked. </param>
-        /// <typeparam name="T"> Any type. </typeparam>
-        /// <returns> True if all fields of <param name="instance"/> equals all fields of <param name="comparator"/>. </returns>
-        public static bool FieldsEquals<T>(this T instance, T comparator)
-            where T : class
-        {
-            return typeof(T).GetFields().Any(e => e.GetValue(instance).Equals(e.GetValue(comparator)));
-        }
-
-        /// <summary>
-        ///      Checks if all public fields of two types are equal.
+        ///      Checks if all fields of two types are equal.
         /// </summary>
         /// <param name="instance"> The first value being checked. </param>
         /// <param name="comparator"> The second value being checked. </param>
         /// <param name="bindingFlags"> The flags that specify what fields are being checked. </param>
         /// <typeparam name="T"> Any type. </typeparam>
         /// <returns> True if all fields of <param name="instance"/> equals all fields of <param name="comparator"/>. </returns>
-        public static bool FieldsEquals<T>(this T instance, T comparator, BindingFlags bindingFlags)
+        public static bool FieldsEquals<T>(this T instance, T comparator, BindingFlags bindingFlags = BindingFlags.Instance | BindingFlags.Public)
             where T : class
         {
             return typeof(T).GetFields(bindingFlags).Any(e => e.GetValue(instance).Equals(e.GetValue(comparator)));
