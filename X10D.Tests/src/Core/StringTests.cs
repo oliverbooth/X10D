6fdee35a1dfceeba94b268582ffe45555d686964
Oliver Booth <me@olivr.me>
2020-10-20 15:08:20 +0100

    🚨 Add tests for latest methods
    AsNullIfEmpty
    AsNullIfWhiteSpace
    Reverse
    WithAlternative

--- X10D.Tests/src/Core/StringTests.cs
+++ X10D.Tests/src/Core/StringTests.cs
@@ -10,12 +10,46 @@
     public class StringTests
     {
         /// <summary>
+        ///     Tests for <see cref="StringExtensions.AsNullIfEmpty" />.
+        /// </summary>
+        [TestMethod]
+        public void AsNullIfEmpty()
+        {
+            Assert.AreEqual(null, string.Empty.AsNullIfEmpty());
+            Assert.AreEqual(null, ((string)null).AsNullIfEmpty());
+            Assert.AreEqual("   ", "   ".AsNullIfEmpty());
+            Assert.AreEqual("foo", "foo".AsNullIfEmpty());
+        }
+
+        /// <summary>
+        ///     Tests for <see cref="StringExtensions.AsNullIfWhiteSpace" />.
+        /// </summary>
+        [TestMethod]
+        public void AsNullIfWhiteSpace()
+        {
+            Assert.AreEqual(null, string.Empty.AsNullIfWhiteSpace());
+            Assert.AreEqual(null, ((string)null).AsNullIfWhiteSpace());
+            Assert.AreEqual(null, "   ".AsNullIfWhiteSpace());
+            Assert.AreEqual("foo", "foo".AsNullIfWhiteSpace());
+        }
+
+        /// <summary>
         ///     Tests for <see cref="StringExtensions.Repeat" />.
         /// </summary>
         [TestMethod]
         public void Repeat()
         {
             Assert.AreEqual("foofoofoofoofoo", "foo".Repeat(5));
+        }
+
+        /// <summary>
+        ///     Tests for <see cref="StringExtensions.Reverse" />.
+        /// </summary>
+        [TestMethod]
+        public void Reverse()
+        {
+            Assert.AreEqual("dlroW olleH", StringExtensions.Reverse("Hello World"));
+            Assert.AreEqual("Foobar", StringExtensions.Reverse("rabooF"));
         }
 
         /// <summary>
@@ -29,6 +63,19 @@
             // ReSharper disable once SuggestVarOrType_Elsewhere
             var arr = str.Split(2).ToArray();
             CollectionAssert.AreEqual(new[] { "He", "ll", "o ", "Wo", "rl", "d" }, arr);
+        }
+
+        /// <summary>
+        ///     Tests for <see cref="StringExtensions.WithAlternative" />.
+        /// </summary>
+        [TestMethod]
+        public void WithAlternative()
+        {
+            Assert.AreEqual("Hello", "Hello".WithAlternative("Discarded"));
+            Assert.AreEqual("Alternative", string.Empty.WithAlternative("Alternative"));
+            Assert.AreEqual("   ", "   ".WithAlternative("Discarded"));
+            Assert.AreEqual("Alternative", "   ".WithAlternative("Alternative", true));
+            Assert.AreEqual("Alternative", ((string)null).WithAlternative("Alternative"));
         }
     }
 }
