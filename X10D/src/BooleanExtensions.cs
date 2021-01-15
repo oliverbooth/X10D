25f582df8dbc15ea2021861916bd2736933d9146
Oliver Booth <me@olivr.me>
2021-01-15 01:55:15 +0000

    🗑 Remove bool logical operator methods
    Resolves #6

--- X10D/src/BooleanExtensions.cs
+++ X10D/src/BooleanExtensions.cs
@@ -6,75 +6,6 @@
     public static class BooleanExtensions
     {
         /// <summary>
-        ///     Performs logical AND on this <see cref="bool" /> and another <see cref="bool" />.
-        /// </summary>
-        /// <param name="value">The boolean.</param>
-        /// <param name="comparison">The boolean comparator.</param>
-        /// <returns>
-        ///     Returns <see langword="true" /> if <paramref name="value" /> AND <paramref name="comparison" />
-        ///     evaluate to <see langword="true" />, or <see langword="false" /> otherwise.
-        /// </returns>
-        public static bool And(this bool value, bool comparison)
-        {
-            return value && comparison;
-        }
-
-        /// <summary>
-        ///     Performs logical NAND on this <see cref="bool" /> and another <see cref="bool" />.
-        /// </summary>
-        /// <param name="value">The boolean.</param>
-        /// <param name="comparison">The boolean comparator.</param>
-        /// <returns>
-        ///     Returns <see langword="true" /> if <paramref name="value" /> NAND <paramref name="comparison" />
-        ///     evaluate to <see langword="true" />, or <see langword="false" /> otherwise.
-        /// </returns>
-        public static bool NAnd(this bool value, bool comparison)
-        {
-            return !(value && comparison);
-        }
-
-        /// <summary>
-        ///     Performs logical NOR on this <see cref="bool" /> and another <see cref="bool" />.
-        /// </summary>
-        /// <param name="value">The boolean.</param>
-        /// <param name="comparison">The boolean comparator.</param>
-        /// <returns>
-        ///     Returns <see langword="true" /> if <paramref name="value" /> NOR <paramref name="comparison" />
-        ///     evaluate to <see langword="true" />, or <see langword="false" /> otherwise.
-        /// </returns>
-        public static bool NOr(this bool value, bool comparison)
-        {
-            return !(value || comparison);
-        }
-
-        /// <summary>
-        ///     Performs logical NOT on this <see cref="bool" />.
-        /// </summary>
-        /// <param name="value">The boolean.</param>
-        /// <returns>
-        ///     Returns <see langword="true" /> if <paramref name="value" /> is <see langword="false" />,
-        ///     or <see langword="false" /> otherwise.
-        /// </returns>
-        public static bool Not(this bool value)
-        {
-            return !value;
-        }
-
-        /// <summary>
-        ///     Performs logical OR on this <see cref="bool" /> and another <see cref="bool" />.
-        /// </summary>
-        /// <param name="value">The boolean.</param>
-        /// <param name="comparison">The boolean comparator.</param>
-        /// <returns>
-        ///     Returns <see langword="true" /> if <paramref name="value" /> OR <paramref name="comparison" />
-        ///     evaluate to <see langword="true" />, or <see langword="false" /> otherwise.
-        /// </returns>
-        public static bool Or(this bool value, bool comparison)
-        {
-            return value || comparison;
-        }
-
-        /// <summary>
         ///     Gets the value of this boolean as represented by <see cref="byte" />.
         /// </summary>
         /// <param name="value">The boolean.</param>
@@ -112,34 +43,6 @@
         public static long ToInt64(this bool value)
         {
             return value.ToInt32();
-        }
-
-        /// <summary>
-        ///     Performs logical XNOR on this <see cref="bool" /> and another <see cref="bool" />.
-        /// </summary>
-        /// <param name="value">The boolean.</param>
-        /// <param name="comparison">The boolean comparator.</param>
-        /// <returns>
-        ///     Returns <see langword="true" /> if <paramref name="value" /> XNOR <paramref name="comparison" />
-        ///     evaluate to <see langword="true" />, or <see langword="false" /> otherwise.
-        /// </returns>
-        public static bool XNOr(this bool value, bool comparison)
-        {
-            return !(value ^ comparison);
-        }
-
-        /// <summary>
-        ///     Performs logical XOR on this <see cref="bool" /> and another <see cref="bool" />.
-        /// </summary>
-        /// <param name="value">The boolean.</param>
-        /// <param name="comparison">The boolean comparator.</param>
-        /// <returns>
-        ///     Returns <see langword="true" /> if <paramref name="value" /> XOR <paramref name="comparison" />
-        ///     evaluate to <see langword="true" />, or <see langword="false" /> otherwise.
-        /// </returns>
-        public static bool XOr(this bool value, bool comparison)
-        {
-            return value ^ comparison;
         }
     }
 }
