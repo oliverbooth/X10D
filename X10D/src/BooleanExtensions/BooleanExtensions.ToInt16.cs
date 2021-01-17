4d08f50a342f92b8ff38e2b71be83388c534d2d8
Oliver Booth <me@olivr.me>
2021-01-17 02:37:59 +0000

    🚚 Move BooleanExtensions to child namespace
    Item for #15, #17 and #19

---
+++ X10D/src/BooleanExtensions/BooleanExtensions.ToInt16.cs
@@ -0,0 +1,19 @@
+﻿namespace X10D.BooleanExtensions
+{
+    public static partial class BooleanExtensions
+    {
+        /// <summary>
+        ///     Converts the current <see cref="bool" /> to a 2-byte signed integer representing its truthiness.
+        /// </summary>
+        /// <param name="value">The value to convert.</param>
+        /// <returns>
+        ///     <c>1</c> if <paramref name="value" /> is <see langword="true"/>
+        ///     -or-
+        ///     <c>0</c> otherwise.
+        /// </returns>
+        public static short ToInt16(this bool value)
+        {
+            return value.ToByte();
+        }
+    }
+}
