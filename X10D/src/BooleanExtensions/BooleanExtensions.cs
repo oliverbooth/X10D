17f6a05c481ed50c5421ab40dc3e90bb9b73cc66
Oliver Booth <me@olivr.me>
2021-01-17 02:37:59 +0000

    🚚 Move BooleanExtensions to child namespace
    Item for #15, #17 and #19

---
+++ X10D/src/BooleanExtensions/BooleanExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace X10D.BooleanExtensions
+{
+    /// <summary>
+    ///     Extension methods for <see cref="bool" />.
+    /// </summary>
+    public static partial class BooleanExtensions
+    {
+    }
+}
