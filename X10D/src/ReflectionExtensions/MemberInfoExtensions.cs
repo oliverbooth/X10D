f510b8ab56f9b5d1b3925d3f3355254af083d33a
Oliver Booth <me@olivr.me>
2021-01-18 16:55:43 +0000

    ✨ Type/MemberInfo extension parity
    Also resolves #25

---
+++ X10D/src/ReflectionExtensions/MemberInfoExtensions.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Globalization;
+using System.Reflection;
+
+namespace X10D.ReflectionExtensions
+{
+    /// <summary>
+    ///     Extension methods for <see cref="MemberInfo" />.
+    /// </summary>
+    public static class MemberInfoExtensions
+    {
+        /// <summary>
+        ///     Returns a value indicating whether or not the current member has been decorated with a specified attribute.
+        /// </summary>
+        /// <param name="member">The member attributes to check.</param>
+        /// <typeparam name="T">The attribute type.</typeparam>
+        /// <returns>
+        ///     <see langword="true" /> if the current member has been decorated with a specified attribute, or
+        ///     <see langword="false" /> otherwise.
+        /// </returns>
+        /// <exception cref="ArgumentNullException"><paramref name="member" /> is <see langword="null" />.</exception>
+        public static bool HasCustomAttribute<T>(this MemberInfo member)
+            where T : Attribute
+        {
+            if (member is null)
+            {
+                throw new ArgumentNullException(nameof(member));
+            }
+
+            return member.HasCustomAttribute(typeof(T));
+        }
+
+        /// <summary>
+        ///     Returns a value indicating whether or not the current member has been decorated with a specified attribute.
+        /// </summary>
+        /// <param name="member">The member attributes to check.</param>
+        /// <param name="attribute">The attribute type.</param>
+        /// <returns>
+        ///     <see langword="true" /> if the current member has been decorated with a specified attribute, or
+        ///     <see langword="false" /> otherwise.
+        /// </returns>
+        /// <exception cref="ArgumentNullException"><paramref name="member" /> is <see langword="null" />.</exception>
+        public static bool HasCustomAttribute(this MemberInfo member, Type attribute)
+        {
+            if (member is null)
+            {
+                throw new ArgumentNullException(nameof(member));
+            }
+
+            if (attribute is null)
+            {
+                throw new ArgumentNullException(nameof(attribute));
+            }
+
+            if (!attribute.Inherits<Attribute>())
+            {
+                throw new ArgumentException(
+                    string.Format(CultureInfo.CurrentCulture, ExceptionMessages.TypeDoesNotInheritAttribute, attribute),
+                    nameof(attribute));
+            }
+
+            return member.GetCustomAttribute(attribute) is not null;
+        }
+        
+        /// <summary>
+        ///     Retrieves a custom attribute that is decorated by the current member, and projects it into to a new form.
+        /// </summary>
+        /// <typeparam name="TAttribute">The attribute type.</typeparam>
+        /// <typeparam name="TReturn">The return type of the <paramref name="selector" /> delegate.</typeparam>
+        /// <param name="type">The member.</param>
+        /// <param name="selector">A transform function to apply to the attribute.</param>
+        /// <returns>
+        ///     An instance of <typeparamref name="TReturn" /> as provided from <paramref name="selector" />.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        ///     <paramref name="type" /> is <see langword="null" />
+        ///     -or-
+        ///     <paramref name="selector" /> is <see langword="null" />.
+        /// </exception>
+        public static TReturn? SelectFromCustomAttribute<TAttribute, TReturn>(this MemberInfo type, Func<TAttribute, TReturn> selector)
+            where TAttribute : Attribute
+        {
+            if (type is null)
+            {
+                throw new ArgumentNullException(nameof(type));
+            }
+
+            if (selector is null)
+            {
+                throw new ArgumentNullException(nameof(selector));
+            }
+
+            return type.GetCustomAttribute<TAttribute>() is { } attribute
+                ? selector(attribute)
+                : default;
+        }
+    }
+}
