ffcf0fee78901c51b6a38ad82f31b5477be8f13b
Oliver Booth <me@olivr.me>
2021-01-18 15:26:14 +0000

    🧹 Code cleanup and XMLdoc fix

--- X10D/src/ListExtensions/ListExtensions.cs
+++ X10D/src/ListExtensions/ListExtensions.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 namespace X10D.ListExtensions
 {
     /// <summary>
-    ///     Extension methods for <see cref="IList{T}" />.
+    ///     Extension methods for <see cref="IList{T}" /> and <see cref="IReadOnlyList{T}" />.
     /// </summary>
     public static class ListExtensions
     {
@@ -21,12 +21,12 @@ namespace X10D.ListExtensions
         ///     <paramref name="source" /> is <see langword="null" />.
         /// </exception>
         /// <example>
-        /// <code lang="csharp">
+        ///     <code lang="csharp">
         /// var list = new List&lt;int&gt; { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
         /// var number = list.Random();
         /// </code>
         /// </example>
-        public static T Random<T>(this IReadOnlyList<T> source, System.Random? random = null)
+        public static T Random<T>(this IReadOnlyList<T> source, Random? random = null)
         {
             if (source is null)
             {
@@ -35,15 +35,15 @@ namespace X10D.ListExtensions
 
             random ??= RandomExtensions.Random;
             return random.NextFrom(source);
-        } 
-        
+        }
+
         /// <summary>
         ///     Reorganizes the elements in a list by implementing a Fisher-Yates shuffle.
         /// </summary>
         /// <typeparam name="T">The element type.</typeparam>
         /// <param name="source">The <see cref="IList{T}" /> to shuffle.</param>
         /// <param name="random">Optional. The <see cref="System.Random" /> instance to use for the shuffling.</param>
-        public static void Shuffle<T>(this IList<T> source, System.Random? random = null)
+        public static void Shuffle<T>(this IList<T> source, Random? random = null)
         {
             if (source is null)
             {
