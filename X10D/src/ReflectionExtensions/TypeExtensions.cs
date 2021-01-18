28f46ecc8d2759f6dc47663d8c92956a424bda33
Oliver Booth <me@olivr.me>
2021-01-18 16:55:43 +0000

    ✨ Type/MemberInfo extension parity
    Also resolves #25

---
+++ X10D/src/ReflectionExtensions/TypeExtensions.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Globalization;
+using System.Reflection;
+
+namespace X10D.ReflectionExtensions
+{
+    /// <summary>
+    ///     Extension methods for <see cref="Type" />.
+    /// </summary>
+    public static class TypeExtensions
+    {
+        /// <summary>
+        ///     Returns a value indicating whether or not the current type has been decorated with a specified attribute.
+        /// </summary>
+        /// <param name="type">The type whose attributes to check.</param>
+        /// <typeparam name="T">The attribute type.</typeparam>
+        /// <returns>
+        ///     <see langword="true" /> if the current type has been decorated with a specified attribute, or
+        ///     <see langword="false" /> otherwise.
+        /// </returns>
+        /// <exception cref="ArgumentNullException"><paramref name="type" /> is <see langword="null" />.</exception>
+        public static bool HasCustomAttribute<T>(this Type type)
+            where T : Attribute
+        {
+            if (type is null)
+            {
+                throw new ArgumentNullException(nameof(type));
+            }
+
+            return type.HasCustomAttribute(typeof(T));
+        }
+
+        /// <summary>
+        ///     Returns a value indicating whether or not the current type has been decorated with a specified attribute.
+        /// </summary>
+        /// <param name="type">The type whose attributes to check.</param>
+        /// <param name="attribute">The attribute type.</param>
+        /// <returns>
+        ///     <see langword="true" /> if the current type has been decorated with a specified attribute, or
+        ///     <see langword="false" /> otherwise.
+        /// </returns>
+        /// <exception cref="ArgumentNullException"><paramref name="type" /> is <see langword="null" />.</exception>
+        public static bool HasCustomAttribute(this Type type, Type attribute)
+        {
+            if (type is null)
+            {
+                throw new ArgumentNullException(nameof(type));
+            }
+
+            if (attribute is null)
+            {
+                throw new ArgumentNullException(nameof(attribute));
+            }
+
+            if (!attribute.Inherits<Attribute>())
+            {
+                throw new ArgumentException(
+                    string.Format(CultureInfo.CurrentCulture, ExceptionMessages.TypeDoesNotInheritAttribute, attribute),
+                    nameof(attribute));
+            }
+
+            return type.GetCustomAttribute(attribute) is not null;
+        }
+
+        /// <summary>
+        ///     Returns a value indicating whether the current type implements a specified interface.
+        /// </summary>
+        /// <param name="value">The type whose interface list to check.</param>
+        /// <typeparam name="T">The interface type.</typeparam>
+        /// <returns><see langword="true" /> if the current exists on the type, or <see langword="false" /> otherwise.</returns>
+        public static bool Implements<T>(this Type value)
+        {
+            return value.Implements(typeof(T));
+        }
+
+        /// <summary>
+        ///     Returns a value indicating whether the current type implements a specified interface.
+        /// </summary>
+        /// <param name="value">The type whose interface list to check.</param>
+        /// <param name="interfaceType">The interface type.</param>
+        /// <returns><see langword="true" /> if the current exists on the type, or <see langword="false" /> otherwise.</returns>
+        public static bool Implements(this Type value, Type interfaceType)
+        {
+            if (value is null)
+            {
+                throw new ArgumentNullException(nameof(value));
+            }
+
+            if (interfaceType is null)
+            {
+                throw new ArgumentNullException(nameof(interfaceType));
+            }
+
+            if (!interfaceType.IsInterface)
+            {
+                throw new ArgumentException(
+                    string.Format(CultureInfo.CurrentCulture, ExceptionMessages.TypeIsNotInterface, interfaceType),
+                    nameof(interfaceType));
+            }
+
+            return Array.IndexOf(value.GetInterfaces(), interfaceType) >= 0;
+        }
+
+        /// <summary>
+        ///     Returns a value indicating whether the current type inherits a specified type.
+        /// </summary>
+        /// <param name="value">The type whose interface list to check.</param>
+        /// <typeparam name="T">The base type.</typeparam>
+        /// <returns>
+        ///     <see langword="true" /> if the current type inherits <typeparamref name="T" />, or <see langword="false" />
+        ///     otherwise.
+        /// </returns>
+        public static bool Inherits<T>(this Type value)
+            where T : class
+        {
+            return value.Inherits(typeof(T));
+        }
+
+        /// <summary>
+        ///     Returns a value indicating whether the current type inherits a specified type.
+        /// </summary>
+        /// <param name="value">The type whose interface list to check.</param>
+        /// <param name="type">The base type.</param>
+        /// <returns>
+        ///     <see langword="true" /> if the current type inherits <paramref name="type" />, or <see langword="false" />
+        ///     otherwise.
+        /// </returns>
+        public static bool Inherits(this Type value, Type type)
+        {
+            if (value is null)
+            {
+                throw new ArgumentNullException(nameof(value));
+            }
+
+            if (type is null)
+            {
+                throw new ArgumentNullException(nameof(type));
+            }
+
+            if (!value.IsClass)
+            {
+                throw new ArgumentException(
+                    string.Format(CultureInfo.CurrentCulture, ExceptionMessages.TypeIsNotClass, value),
+                    nameof(value));
+            }
+
+            if (!type.IsClass)
+            {
+                throw new ArgumentException(
+                    string.Format(CultureInfo.CurrentCulture, ExceptionMessages.TypeIsNotClass, type),
+                    nameof(type));
+            }
+
+            return value.IsSubclassOf(type);
+        }
+
+        /// <summary>
+        ///     Retrieves a custom attribute that is decorated by the current type, and projects it into to a new form.
+        /// </summary>
+        /// <typeparam name="TAttribute">The attribute type.</typeparam>
+        /// <typeparam name="TReturn">The return type of the <paramref name="selector" /> delegate.</typeparam>
+        /// <param name="type">The type.</param>
+        /// <param name="selector">A transform function to apply to the attribute.</param>
+        /// <returns>
+        ///     An instance of <typeparamref name="TReturn" /> as provided from <paramref name="selector" />.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        ///     <paramref name="type" /> is <see langword="null" />
+        ///     -or-
+        ///     <paramref name="selector" /> is <see langword="null" />.
+        /// </exception>
+        public static TReturn? SelectFromCustomAttribute<TAttribute, TReturn>(this Type type, Func<TAttribute, TReturn> selector)
+            where TAttribute : Attribute
+        {
+            if (type is null)
+            {
+                throw new ArgumentNullException(nameof(type));
+            }
+
+            if (selector is null)
+            {
+                throw new ArgumentNullException(nameof(selector));
+            }
+
+            return type.GetCustomAttribute<TAttribute>() is { } attribute
+                ? selector(attribute)
+                : default;
+        }
+    }
+}
