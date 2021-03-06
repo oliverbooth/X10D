4d31eee600811216e5c10dd79f19104ebf700740
Oliver Booth <me@olivr.me>
2021-03-06 17:27:34 +0000

    (#28) (#29) Use Span where possible
    - Delegate argument validation to Validate class
    - Fix ReadDecimal
    - Add Write(decimal)

---
+++ X10D/src/Validate.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace X10D
+{
+    internal static class Validate
+    {
+        public static void IsDefined<T>(T value, string argumentName)
+            where T : Enum
+        {
+            if (!Enum.IsDefined(typeof(T), value))
+            {
+                throw new ArgumentOutOfRangeException(argumentName);
+            }
+        }
+        
+        public static void IsNotNull<T>(T argument, string argumentName)
+        {
+            if (argument is null)
+            {
+                throw new ArgumentNullException(argumentName);
+            }
+        }
+    }
+}
