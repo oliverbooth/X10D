f1b28789d94fb3d680b4285dab2b129c4f924f3b
Oliver Booth <me@olivr.me>
2021-03-07 18:09:27 +0000

    (#7) Move CharExtensions to child namespace
    - Deprecate Random methods in favour of RandomExtensions methods

---
+++ X10D/src/CharExtensions/CharExtensions.cs
@@ -0,0 +1,21 @@
+﻿namespace X10D.CharExtensions
+{
+    /// <summary>
+    ///     Extension methods for <see cref="char" />.
+    /// </summary>
+    public static class CharExtensions
+    {
+        /// <summary>
+        ///     Returns a string composed of the current character repeated a specified number of times.
+        /// </summary>
+        /// <param name="value">The character to repeat.</param>
+        /// <param name="count">The number of times to repeat.</param>
+        /// <returns>
+        ///     A <see cref="string" /> composed of <paramref name="value" /> repeated <paramref name="count" /> times.
+        /// </returns>
+        public static string Repeat(this char value, int count)
+        {
+            return new(value, count);
+        }
+    }
+}
