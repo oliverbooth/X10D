41d09695e067d4fa4a902bd685d587f141967534
Oliver Booth <me@olivr.me>
2021-03-07 22:13:55 +0000

    (#7) Move Double/Single to child namespace

--- X10D/src/SingleExtensions/SingleExtensions.cs
+++ X10D/src/SingleExtensions/SingleExtensions.cs
@@ -1,70 +1,55 @@
 ﻿using System;
+using X10D.DoubleExtensions;
 
-namespace X10D
+namespace X10D.SingleExtensions
 {
     /// <summary>
-    ///     Extension methods for <see cref="double" />.
+    ///     Extension methods for <see cref="float" />.
     /// </summary>
-    public static class DoubleExtensions
+    public static class SingleExtensions
     {
-        /// <summary>
-        ///     Clamps a value between a minimum and a maximum value.
-        /// </summary>
-        /// <param name="value">The value to clamp.</param>
-        /// <param name="min">The minimum value.</param>
-        /// <param name="max">The maximum value.</param>
-        /// <returns>
-        ///     Returns <paramref name="max" /> if <paramref name="value" /> is greater than it,
-        ///     <paramref name="min" /> if <paramref name="value" /> is less than it,
-        ///     or <paramref name="value" /> itself otherwise.
-        /// </returns>
-        public static double Clamp(this double value, double min, double max)
-        {
-            return Math.Min(Math.Max(value, min), max);
-        }
-
         /// <summary>
         ///     Converts an angle from degrees to radians.
         /// </summary>
         /// <param name="angle">The angle in degrees.</param>
         /// <returns>Returns <paramref name="angle" /> in radians.</returns>
-        public static double DegreesToRadians(this double angle)
+        public static float DegreesToRadians(this float angle)
         {
-            return (Math.PI * angle) / 180.0;
+            return (float)((double)angle).DegreesToRadians();
         }
 
         /// <summary>
-        ///     Converts the <see cref="double" /> to a <see cref="byte" />[].
+        ///     Converts the <see cref="float" /> to a <see cref="byte" />[].
         /// </summary>
         /// <param name="number">The number to convert.</param>
         /// <returns>Returns a <see cref="byte" />[].</returns>
-        public static byte[] GetBytes(this double number)
+        public static byte[] GetBytes(this float number)
         {
             return BitConverter.GetBytes(number);
         }
 
         /// <summary>
-        ///     Determines if the <see cref="double" /> is even.
+        ///     Determines if the <see cref="float" /> is even.
         /// </summary>
         /// <param name="number">The number.</param>
         /// <returns>
         ///     Returns <see langword="true" /> if <paramref name="number" /> is even, <see langword="false" />
         ///     otherwise.
         /// </returns>
-        public static bool IsEven(this double number)
+        public static bool IsEven(this float number)
         {
-            return Math.Abs(number % 2.0) < double.Epsilon;
+            return ((double)number).IsEven();
         }
 
         /// <summary>
-        ///     Determines if the <see cref="double" /> is odd.
+        ///     Determines if the <see cref="float" /> is odd.
         /// </summary>
         /// <param name="number">The number.</param>
         /// <returns>
         ///     Returns <see langword="true" /> if <paramref name="number" /> is odd, <see langword="false" />
         ///     otherwise.
         /// </returns>
-        public static bool IsOdd(this double number)
+        public static bool IsOdd(this float number)
         {
             return !number.IsEven();
         }
@@ -74,9 +59,9 @@ namespace X10D
         /// </summary>
         /// <param name="angle">The angle in radians.</param>
         /// <returns>Returns <paramref name="angle" /> in degrees.</returns>
-        public static double RadiansToDegrees(this double angle)
+        public static float RadiansToDegrees(this float angle)
         {
-            return angle * (180.0 / Math.PI);
+            return (float)((double)angle).RadiansToDegrees();
         }
 
         /// <summary>
@@ -85,9 +70,9 @@ namespace X10D
         /// <param name="v">The value to round.</param>
         /// <param name="nearest">The nearest value.</param>
         /// <returns>Returns the rounded value.</returns>
-        public static double Round(this double v, double nearest = 1)
+        public static float Round(this float v, float nearest = 1)
         {
-            return Math.Round(v / nearest) * nearest;
+            return (float)((double)v).Round(nearest);
         }
     }
 }
