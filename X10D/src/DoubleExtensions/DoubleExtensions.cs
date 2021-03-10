1b811cb18d47a463fdcd35216ab1cb3630d5df73
Oliver Booth <me@olivr.me>
2021-03-10 12:17:23 +0000

    (#38) Add Single/Double.LerpFrom/To/With

--- X10D/src/DoubleExtensions/DoubleExtensions.cs
+++ X10D/src/DoubleExtensions/DoubleExtensions.cs
@@ -54,6 +54,51 @@ namespace X10D.DoubleExtensions
         }
 
         /// <summary>
+        ///     Linearly interpolates to the current value from a specified source using a specified alpha.
+        /// </summary>
+        /// <param name="target">The interpolation target.</param>
+        /// <param name="value">The interpolation source.</param>
+        /// <param name="alpha">The interpolation alpha.</param>
+        /// <returns>
+        ///     The interpolation result as determined by <c>(1 - <paramref name="alpha" />) * <paramref name="value" /> +
+        ///     <paramref name="alpha" /> * <paramref name="target" /></c>.
+        /// </returns>
+        public static double LerpFrom(this double target, double value, double alpha)
+        {
+            return LerpInternal(value, target, alpha);
+        }
+
+        /// <summary>
+        ///     Linearly interpolates from the current value to a specified target using a specified alpha.
+        /// </summary>
+        /// <param name="value">The interpolation source.</param>
+        /// <param name="target">The interpolation target.</param>
+        /// <param name="alpha">The interpolation alpha.</param>
+        /// <returns>
+        ///     The interpolation result as determined by <c>(1 - <paramref name="alpha" />) * <paramref name="value" /> +
+        ///     <paramref name="alpha" /> * <paramref name="target" /></c>.
+        /// </returns>
+        public static double LerpTo(this double value, double target, double alpha)
+        {
+            return LerpInternal(value, target, alpha);
+        }
+
+        /// <summary>
+        ///     Linearly interpolates to a specified target from a specified source, using the current value as the alpha value.
+        /// </summary>
+        /// <param name="alpha">The interpolation alpha.</param>
+        /// <param name="value">The interpolation source.</param>
+        /// <param name="target">The interpolation target.</param>
+        /// <returns>
+        ///     The interpolation result as determined by <c>(1 - <paramref name="alpha" />) * <paramref name="value" /> +
+        ///     <paramref name="alpha" /> * <paramref name="target" /></c>.
+        /// </returns>
+        public static double LerpWith(this double alpha, double value, double target)
+        {
+            return LerpInternal(value, target, alpha);
+        }
+
+        /// <summary>
         ///     Converts an angle from radians to degrees.
         /// </summary>
         /// <param name="angle">The angle in radians.</param>
@@ -72,6 +117,14 @@ namespace X10D.DoubleExtensions
         public static double Round(this double v, double nearest = 1)
         {
             return Math.Round(v / nearest) * nearest;
+        }
+
+        private static double LerpInternal(double a, double b, double t)
+        {
+            // rookie mistake: a + t * (b - a)
+            
+            // "precise" method: (1 - t) * a + t * b
+            return (1 - t) * a + t * b;
         }
     }
 }
