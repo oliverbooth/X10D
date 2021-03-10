aaa1abf5a4bcc5e081ccd81f814332507e8cb153
Oliver Booth <me@olivr.me>
2021-03-10 11:21:49 +0000

    (#7) Move StringExtensions to child namespace

--- X10D/src/StringExtensions/StringExtensions.cs
+++ X10D/src/StringExtensions/StringExtensions.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace X10D
+namespace X10D.StringExtensions
 {
     /// <summary>
     ///     Extension methods for <see cref="string" />.
