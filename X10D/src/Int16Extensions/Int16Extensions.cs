481fcabe4149c6f39d828614daea8d060f7e4a63
Oliver Booth <me@olivr.me>
2021-03-16 14:46:49 +0000

    (#38) Add short/int/long.Lerp

--- X10D/src/Int16Extensions/Int16Extensions.cs
+++ X10D/src/Int16Extensions/Int16Extensions.cs
@@ -83,6 +83,96 @@ namespace X10D.Int16Extensions
         }
 
         /// <summary>
+        ///     Linearly interpolates to the current value from a specified source using a specified alpha.
+        /// </summary>
+        /// <param name="target">The interpolation target.</param>
+        /// <param name="value">The interpolation source.</param>
+        /// <param name="alpha">The interpolation alpha.</param>
+        /// <returns>
+        ///     The interpolation result as determined by <c>(1 - <paramref name="alpha" />) * <paramref name="value" /> +
+        ///     <paramref name="alpha" /> * <paramref name="target" /></c>.
+        /// </returns>
+        public static double LerpFrom(this short target, double value, double alpha)
+        {
+            return DoubleExtensions.DoubleExtensions.LerpInternal(value, target, alpha);
+        }
+
+        /// <summary>
+        ///     Linearly interpolates from the current value to a specified target using a specified alpha.
+        /// </summary>
+        /// <param name="value">The interpolation source.</param>
+        /// <param name="target">The interpolation target.</param>
+        /// <param name="alpha">The interpolation alpha.</param>
+        /// <returns>
+        ///     The interpolation result as determined by <c>(1 - <paramref name="alpha" />) * <paramref name="value" /> +
+        ///     <paramref name="alpha" /> * <paramref name="target" /></c>.
+        /// </returns>
+        public static double LerpTo(this short value, double target, double alpha)
+        {
+            return DoubleExtensions.DoubleExtensions.LerpInternal(value, target, alpha);
+        }
+
+        /// <summary>
+        ///     Linearly interpolates to a specified target from a specified source, using the current value as the alpha value.
+        /// </summary>
+        /// <param name="alpha">The interpolation alpha.</param>
+        /// <param name="value">The interpolation source.</param>
+        /// <param name="target">The interpolation target.</param>
+        /// <returns>
+        ///     The interpolation result as determined by <c>(1 - <paramref name="alpha" />) * <paramref name="value" /> +
+        ///     <paramref name="alpha" /> * <paramref name="target" /></c>.
+        /// </returns>
+        public static double LerpWith(this short alpha, double value, double target)
+        {
+            return DoubleExtensions.DoubleExtensions.LerpInternal(value, target, alpha);
+        }
+
+        /// <summary>
+        ///     Linearly interpolates to the current value from a specified source using a specified alpha.
+        /// </summary>
+        /// <param name="target">The interpolation target.</param>
+        /// <param name="value">The interpolation source.</param>
+        /// <param name="alpha">The interpolation alpha.</param>
+        /// <returns>
+        ///     The interpolation result as determined by <c>(1 - <paramref name="alpha" />) * <paramref name="value" /> +
+        ///     <paramref name="alpha" /> * <paramref name="target" /></c>.
+        /// </returns>
+        public static float LerpFrom(this short target, float value, float alpha)
+        {
+            return SingleExtensions.SingleExtensions.LerpInternal(value, target, alpha);
+        }
+
+        /// <summary>
+        ///     Linearly interpolates from the current value to a specified target using a specified alpha.
+        /// </summary>
+        /// <param name="value">The interpolation source.</param>
+        /// <param name="target">The interpolation target.</param>
+        /// <param name="alpha">The interpolation alpha.</param>
+        /// <returns>
+        ///     The interpolation result as determined by <c>(1 - <paramref name="alpha" />) * <paramref name="value" /> +
+        ///     <paramref name="alpha" /> * <paramref name="target" /></c>.
+        /// </returns>
+        public static float LerpTo(this short value, float target, float alpha)
+        {
+            return SingleExtensions.SingleExtensions.LerpInternal(value, target, alpha);
+        }
+
+        /// <summary>
+        ///     Linearly interpolates to a specified target from a specified source, using the current value as the alpha value.
+        /// </summary>
+        /// <param name="alpha">The interpolation alpha.</param>
+        /// <param name="value">The interpolation source.</param>
+        /// <param name="target">The interpolation target.</param>
+        /// <returns>
+        ///     The interpolation result as determined by <c>(1 - <paramref name="alpha" />) * <paramref name="value" /> +
+        ///     <paramref name="alpha" /> * <paramref name="target" /></c>.
+        /// </returns>
+        public static float LerpWith(this short alpha, float value, float target)
+        {
+            return SingleExtensions.SingleExtensions.LerpInternal(value, target, alpha);
+        }
+
+        /// <summary>
         ///     Converts the value of the current 16-bit signed integer to an equivalent <see cref="bool" /> value.
         /// </summary>
         /// <param name="value">The value to convert.</param>
--- X10D/src/Int64Extensions/Int64Extensions.cs
+++ X10D/src/Int64Extensions/Int64Extensions.cs
@@ -106,6 +106,96 @@ namespace X10D.Int64Extensions
         }
 
         /// <summary>
+        ///     Linearly interpolates to the current value from a specified source using a specified alpha.
+        /// </summary>
+        /// <param name="target">The interpolation target.</param>
+        /// <param name="value">The interpolation source.</param>
+        /// <param name="alpha">The interpolation alpha.</param>
+        /// <returns>
+        ///     The interpolation result as determined by <c>(1 - <paramref name="alpha" />) * <paramref name="value" /> +
+        ///     <paramref name="alpha" /> * <paramref name="target" /></c>.
+        /// </returns>
+        public static double LerpFrom(this long target, double value, double alpha)
+        {
+            return DoubleExtensions.DoubleExtensions.LerpInternal(value, target, alpha);
+        }
+
+        /// <summary>
+        ///     Linearly interpolates from the current value to a specified target using a specified alpha.
+        /// </summary>
+        /// <param name="value">The interpolation source.</param>
+        /// <param name="target">The interpolation target.</param>
+        /// <param name="alpha">The interpolation alpha.</param>
+        /// <returns>
+        ///     The interpolation result as determined by <c>(1 - <paramref name="alpha" />) * <paramref name="value" /> +
+        ///     <paramref name="alpha" /> * <paramref name="target" /></c>.
+        /// </returns>
+        public static double LerpTo(this long value, double target, double alpha)
+        {
+            return DoubleExtensions.DoubleExtensions.LerpInternal(value, target, alpha);
+        }
+
+        /// <summary>
+        ///     Linearly interpolates to a specified target from a specified source, using the current value as the alpha value.
+        /// </summary>
+        /// <param name="alpha">The interpolation alpha.</param>
+        /// <param name="value">The interpolation source.</param>
+        /// <param name="target">The interpolation target.</param>
+        /// <returns>
+        ///     The interpolation result as determined by <c>(1 - <paramref name="alpha" />) * <paramref name="value" /> +
+        ///     <paramref name="alpha" /> * <paramref name="target" /></c>.
+        /// </returns>
+        public static double LerpWith(this long alpha, double value, double target)
+        {
+            return DoubleExtensions.DoubleExtensions.LerpInternal(value, target, alpha);
+        }
+
+        /// <summary>
+        ///     Linearly interpolates to the current value from a specified source using a specified alpha.
+        /// </summary>
+        /// <param name="target">The interpolation target.</param>
+        /// <param name="value">The interpolation source.</param>
+        /// <param name="alpha">The interpolation alpha.</param>
+        /// <returns>
+        ///     The interpolation result as determined by <c>(1 - <paramref name="alpha" />) * <paramref name="value" /> +
+        ///     <paramref name="alpha" /> * <paramref name="target" /></c>.
+        /// </returns>
+        public static float LerpFrom(this long target, float value, float alpha)
+        {
+            return SingleExtensions.SingleExtensions.LerpInternal(value, target, alpha);
+        }
+
+        /// <summary>
+        ///     Linearly interpolates from the current value to a specified target using a specified alpha.
+        /// </summary>
+        /// <param name="value">The interpolation source.</param>
+        /// <param name="target">The interpolation target.</param>
+        /// <param name="alpha">The interpolation alpha.</param>
+        /// <returns>
+        ///     The interpolation result as determined by <c>(1 - <paramref name="alpha" />) * <paramref name="value" /> +
+        ///     <paramref name="alpha" /> * <paramref name="target" /></c>.
+        /// </returns>
+        public static float LerpTo(this long value, float target, float alpha)
+        {
+            return SingleExtensions.SingleExtensions.LerpInternal(value, target, alpha);
+        }
+
+        /// <summary>
+        ///     Linearly interpolates to a specified target from a specified source, using the current value as the alpha value.
+        /// </summary>
+        /// <param name="alpha">The interpolation alpha.</param>
+        /// <param name="value">The interpolation source.</param>
+        /// <param name="target">The interpolation target.</param>
+        /// <returns>
+        ///     The interpolation result as determined by <c>(1 - <paramref name="alpha" />) * <paramref name="value" /> +
+        ///     <paramref name="alpha" /> * <paramref name="target" /></c>.
+        /// </returns>
+        public static float LerpWith(this long alpha, float value, float target)
+        {
+            return SingleExtensions.SingleExtensions.LerpInternal(value, target, alpha);
+        }
+
+        /// <summary>
         ///     Converts the value of the current 64-bit signed integer to an equivalent Boolean value.
         /// </summary>
         /// <param name="value">The value to convert.</param>
