f59f7ad3114c503df712f429c4d0c18dc5e81489
Oliver Booth <me@olivr.me>
2021-06-27 13:19:55 +0100

    (#15) Add xmldoc to Endianness enum fields

--- X10D/src/Endianness.cs
+++ X10D/src/Endianness.cs
@@ -7,9 +7,15 @@ namespace X10D
     /// </summary>
     public enum Endianness
     {
+        /// <summary>
+        ///     The value should be read as though it uses little endian encoding.
+        /// </summary>
         [Description("The value should be read as though it uses little endian encoding.")]
         LittleEndian,
 
+        /// <summary>
+        ///     The value should be read as though it uses big endian encoding.
+        /// </summary>
         [Description("The value should be read as though it uses big endian encoding.")]
         BigEndian
     }
