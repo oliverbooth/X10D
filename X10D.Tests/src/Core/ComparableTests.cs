64bcb34a11020b1eeea71ad716ed1aee423770b7
Oliver Booth <me@olivr.me>
2022-02-14 16:24:14 +0000

    Introduce Between clusivity

--- X10D.Tests/src/Core/ComparableTests.cs
+++ X10D.Tests/src/Core/ComparableTests.cs
@@ -9,14 +9,26 @@ namespace X10D.Tests.Core
     public class ComparableTests
     {
         /// <summary>
-        ///     Tests for <see cref="ComparableExtensions.Between{T}" />.
+        ///     Tests <see cref="ComparableExtensions.Between{T1, T2, T3}" />
         /// </summary>
         [TestMethod]
         public void Between()
         {
-            Assert.IsTrue(5.Between(2, 7));
-            Assert.IsTrue(10.Between(9, 11));
-            Assert.IsFalse(100.Between(80, 99));
+            const int lower = 5;
+            const int upper = 15;
+            const int value = 10;
+
+            Assert.IsTrue(value.Between(lower, upper), "value.Between(lower, upper)");
+            Assert.IsFalse(lower.Between(value, upper), "lower.Between(value, upper)");
+            Assert.IsFalse(upper.Between(lower, value), "upper.Between(lower, value)");
+            
+            Assert.IsTrue(upper.Between(lower, upper, Clusivity.UpperInclusive), "upper.Between(lower, upper, Clusivity.UpperInclusive)");
+            Assert.IsTrue(upper.Between(lower, upper, Clusivity.Inclusive), "upper.Between(lower, upper, Clusivity.Inclusive)");
+            Assert.IsFalse(upper.Between(lower, upper, Clusivity.LowerInclusive), "upper.Between(lower, upper, Clusivity.LowerInclusive)");
+            
+            Assert.IsTrue(lower.Between(lower, upper, Clusivity.LowerInclusive), "lower.Between(lower, upper, Clusivity.LowerInclusive)");
+            Assert.IsTrue(lower.Between(lower, upper, Clusivity.Inclusive), "lower.Between(lower, upper, Clusivity.Inclusive)");
+            Assert.IsFalse(lower.Between(lower, upper, Clusivity.UpperInclusive), "lower.Between(lower, upper, Clusivity.UpperInclusive)");
         }
     }
 }
---
+++ X10D/src/ComparableExtensions/Clusivity.cs
@@ -0,0 +1,28 @@
+﻿namespace X10D;
+
+/// <summary>
+///     Provides options for <see cref="ComparableExtensions.Between{T1, T2, T3}" /> clusivity.
+/// </summary>
+[Flags]
+public enum Clusivity : byte
+{
+    /// <summary>
+    ///     Indicates that the comparison will be exclusive. 
+    /// </summary>
+    Exclusive,
+    
+    /// <summary>
+    ///     Indicates that the comparison will treat the upper bound as exclusive. 
+    /// </summary>
+    UpperInclusive,
+    
+    /// <summary>
+    ///     Indicates that the comparison will treat the lower bound as exclusive. 
+    /// </summary>
+    LowerInclusive,
+    
+    /// <summary>
+    ///     Indicates that the comparison will treat both the upper and lower bound as exclusive. 
+    /// </summary>
+    Inclusive
+}
