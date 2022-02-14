c014b7d41063bb6dd8dc24b75431f031578896ff
Oliver Booth <me@olivr.me>
2022-02-14 16:32:30 +0000

    (#14) Use explicit type
    Use implicit using

--- X10D/src/ByteExtensions/SByteExtensions.cs
+++ X10D/src/ByteExtensions/SByteExtensions.cs
@@ -1,5 +1,3 @@
-using System;
-
 namespace X10D
 {
     /// <summary>
--- X10D/src/DateTimeExtensions/DateTimeOffsetExtensions.cs
+++ X10D/src/DateTimeExtensions/DateTimeOffsetExtensions.cs
@@ -1,6 +1,4 @@
-using System;
-
-namespace X10D
+﻿namespace X10D
 {
     /// <summary>
     ///     Extension methods for <see cref="DateTimeOffset" />.
