60440105965b1c6e25a567cba6c7cba317e54fca
Oliver Booth <me@olivr.me>
2022-04-20 18:16:59 +0100

    Separate unwrapped Enum.Next/Previous to Unchecked overloads

--- X10D/src/EnumExtensions/EnumExtensions.cs
+++ X10D/src/EnumExtensions/EnumExtensions.cs
@@ -6,44 +6,78 @@
 public static class EnumExtensions
 {
     /// <summary>
-    ///     Returns the next member defined in a specified enum.
+    ///     Returns the value which is defined proceeding this value in the enumeration.  
     /// </summary>
-    /// <typeparam name="T">The enum type.</typeparam>
-    /// <param name="source">The enum value which should be used as the starting point.</param>
-    /// <param name="wrap">
-    ///     Optional. <see langword="true" /> if the final value of <typeparamref name="T" /> should wrap around to the first
-    ///     value; otherwise, <see langword="false" />. Defaults to <see langword="true" />.
-    /// </param>
+    /// <typeparam name="T">The type of the enumeration.</typeparam>
+    /// <param name="value">The value whose proceeding value to retrieve.</param>
     /// <returns>
-    ///     A value of <typeparamref name="T" /> that is considered to be the next value defined after
-    ///     <paramref name="source" />.
+    ///     A value of <typeparamref name="T" /> that is considered to be the next value defined after <paramref name="value" />,
+    ///     or the first value if <paramref name="value" /> is the final field of the enumeration.
     /// </returns>
-    public static T Next<T>(this T source, bool wrap = true)
+    public static T Next<T>(this T value)
         where T : struct, Enum
     {
-        var array = Enum.GetValues<T>();
-        int index = Array.IndexOf(array, source) + 1;
-        return array.Length == index ? array[wrap ? 0 : index - 1] : array[index];
+        T[] values = Enum.GetValues<T>();
+        int index = Array.IndexOf(values, value) + 1;
+        index %= values.Length;
+        return values[index];
     }
 
     /// <summary>
-    ///     Returns the previous member defined in a specified enum.
+    ///     Returns the value which is defined proceeding this value in the enumeration.  
     /// </summary>
-    /// <typeparam name="T">The enum type.</typeparam>
-    /// <param name="source">The enum value which should be used as the starting point.</param>
-    /// <param name="wrap">
-    ///     Optional. <see langword="true" /> if the first value of <typeparamref name="T" /> should wrap around to the final
-    ///     value; otherwise, <see langword="false" />. Defaults to <see langword="true" />.
-    /// </param>
+    /// <typeparam name="T">The type of the enumeration.</typeparam>
+    /// <param name="value">The value whose proceeding value to retrieve.</param>
     /// <returns>
-    ///     A value of <typeparamref name="T" /> that is considered to be the previous value defined before
-    ///     <paramref name="source" />.
+    ///     A value of <typeparamref name="T" /> that is considered to be the next value defined after
+    ///     <paramref name="value" />.
     /// </returns>
-    public static T Previous<T>(this T source, bool wrap = true)
+    /// <exception cref="IndexOutOfRangeException"><paramref name="value" /> is the final field of the enumeration.</exception>
+    public static T NextUnchecked<T>(this T value)
         where T : struct, Enum
     {
-        var array = Enum.GetValues<T>();
-        int index = Array.IndexOf(array, source) - 1;
-        return index < 0 ? array[wrap ? array.Length - 1 : 0] : array[index];
+        T[] values = Enum.GetValues<T>();
+        int index = Array.IndexOf(values, value) + 1;
+        return values[index];
+    }
+
+    /// <summary>
+    ///     Returns the value which is defined preceeding this value in the enumeration.  
+    /// </summary>
+    /// <typeparam name="T">The type of the enumeration.</typeparam>
+    /// <param name="value">The value whose preceeding value to retrieve.</param>
+    /// <returns>
+    ///     A value of <typeparamref name="T" /> that is considered to be the previous value defined after
+    ///     <paramref name="value" />, or the last value if <paramref name="value" /> is the first field of the enumeration.
+    /// </returns>
+    public static T Previous<T>(this T value)
+        where T : struct, Enum
+    {
+        T[] values = Enum.GetValues<T>();
+        int index = Array.IndexOf(values, value) - 1;
+        int length = values.Length;
+
+        // negative modulo is not supported in C#. workaround: https://stackoverflow.com/a/1082938/1467293
+        // sure, simply checking for index < 0 is enough, but this expression is so fucking cool!
+        index = (index % length + length) % length;
+        return values[index];
+    }
+
+    /// <summary>
+    ///     Returns the value which is defined preceeding this value in the enumeration.  
+    /// </summary>
+    /// <typeparam name="T">The type of the enumeration.</typeparam>
+    /// <param name="value">The value whose preceeding value to retrieve.</param>
+    /// <returns>
+    ///     A value of <typeparamref name="T" /> that is considered to be the previous value defined after
+    ///     <paramref name="value" />, or the last value if <paramref name="value" /> is the first field of the enumeration.
+    /// </returns>
+    /// <exception cref="IndexOutOfRangeException"><paramref name="value" /> is the first field of the enumeration.</exception>
+    public static T PreviousUnchecked<T>(this T value)
+        where T : struct, Enum
+    {
+        T[] values = Enum.GetValues<T>();
+        int index = Array.IndexOf(values, value) - 1;
+        return values[index];
     }
 }
