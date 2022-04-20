39f3725af333c9c81f51bd385363cab218379a81
Oliver Booth <me@olivr.me>
2022-04-20 16:47:15 +0100

    [ci skip] Add test for Enumerable.Shuffled

--- X10D.Tests/src/Core/EnumerableTests.cs
+++ X10D.Tests/src/Core/EnumerableTests.cs
@@ -5,4 +5,11 @@ namespace X10D.Tests.Core;
 [TestClass]
 public partial class EnumerableTests
 {
+    [TestMethod]
+    [DataRow(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)]
+    public void Shuffled(params int[] source)
+    {
+        int[] shuffled = source.Shuffled().ToArray(); // ToArray forces type match
+        CollectionAssert.AreNotEqual(source, shuffled);
+    }
 }
