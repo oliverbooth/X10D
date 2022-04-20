1775f1c8cf30e7e71bbe81a93e9075d19b8303d0
Oliver Booth <me@olivr.me>
2022-04-20 23:14:16 +0100

    Add AsArray/AsEnumerable (#47)

--- X10D.Tests/src/Core/EnumerableTests.cs
+++ X10D.Tests/src/Core/EnumerableTests.cs
@@ -6,6 +6,18 @@ namespace X10D.Tests.Core;
 public partial class EnumerableTests
 {
     [TestMethod]
+    [DataRow(1)]
+    [DataRow("f")]
+    [DataRow(true)]
+    public void AsEnumerable(object o)
+    {
+        IEnumerable<object> array = o.AsEnumerable().ToArray(); // prevent multiple enumeration of IEnumerable
+        Assert.IsNotNull(array);
+        Assert.IsTrue(array.Count() == 1);
+        Assert.AreEqual(o, array.ElementAt(0));
+    }
+    
+    [TestMethod]
     [DataRow(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)]
     public void Shuffled(params int[] source)
     {
