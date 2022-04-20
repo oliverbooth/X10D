33e5f81b08fadcaed39a30c4dd0582e779810d39
Oliver Booth <me@olivr.me>
2022-04-20 16:26:15 +0100

    Remove IEnumerable<T>.Split(int)
    This functionality has been introduced in .NET 6 with the Chunk method: https://docs.microsoft.com/en-us/dotnet/api/system.linq.enumerable.chunk?view=net-6.0

--- X10D/src/EnumerableExtensions/EnumerableExtensions.cs
+++ X10D/src/EnumerableExtensions/EnumerableExtensions.cs
@@ -17,40 +17,4 @@ public static partial class EnumerableExtensions
         list.Shuffle(random);
         return list.AsReadOnly();
     }
-
-    /// <summary>
-    ///     Splits <paramref name="source" /> into chunks of size <paramref name="chunkSize" />.
-    /// </summary>
-    /// <typeparam name="T">Any type.</typeparam>
-    /// <param name="source">The collection to split.</param>
-    /// <param name="chunkSize">The maximum length of the nested collection.</param>
-    /// <returns>
-    ///     An <see cref="IEnumerable{T}" /> containing an <see cref="IEnumerable{T}" /> of <typeparamref name="T" />
-    ///     whose lengths are no greater than <paramref name="chunkSize" />.
-    /// </returns>
-    public static IEnumerable<IEnumerable<T>> Split<T>(this IEnumerable<T> source, int chunkSize)
-    {
-        if (source is null)
-        {
-            throw new ArgumentNullException(nameof(source));
-        }
-
-        var buffer = new List<T>(chunkSize);
-
-        foreach (var item in source)
-        {
-            buffer.Add(item);
-
-            if (buffer.Count >= chunkSize)
-            {
-                yield return buffer;
-                buffer.Clear();
-            }
-        }
-
-        if (buffer.Count > 0)
-        {
-            yield return buffer;
-        }
-    }
 }
