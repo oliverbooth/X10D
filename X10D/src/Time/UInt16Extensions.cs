a074ed3663caf3479b4022746edb5a20c608ce59
Oliver Booth <me@olivr.me>
2022-04-21 20:54:17 +0100

    Add time-related extension methods

---
+++ X10D/src/Time/UInt16Extensions.cs
@@ -0,0 +1,88 @@
+﻿namespace X10D.Time;
+
+/// <summary>
+///     Time-related extension methods for <see cref="ushort" />.
+/// </summary>
+[CLSCompliant(false)]
+public static class UInt16Extensions
+{
+    /// <summary>
+    ///     Returns a <see cref="TimeSpan" /> that represents this value as the number of ticks.
+    /// </summary>
+    /// <param name="value">The duration, in ticks.</param>
+    /// <returns>A <see cref="TimeSpan" /> whose <see cref="TimeSpan.Ticks" /> will equal <paramref name="value" />.</returns>
+    public static TimeSpan Ticks(this ushort value)
+    {
+        return TimeSpan.FromTicks(value);
+    }
+
+    /// <summary>
+    ///     Returns a <see cref="TimeSpan" /> that represents this value as the number of milliseconds.
+    /// </summary>
+    /// <param name="value">The duration, in milliseconds.</param>
+    /// <returns>
+    ///     A <see cref="TimeSpan" /> whose <see cref="TimeSpan.TotalMilliseconds" /> will equal <paramref name="value" />.
+    /// </returns>
+    public static TimeSpan Milliseconds(this ushort value)
+    {
+        return TimeSpan.FromMilliseconds(value);
+    }
+
+    /// <summary>
+    ///     Returns a <see cref="TimeSpan" /> that represents this value as the number of seconds.
+    /// </summary>
+    /// <param name="value">The duration, in seconds.</param>
+    /// <returns>
+    ///     A <see cref="TimeSpan" /> whose <see cref="TimeSpan.TotalSeconds" /> will equal <paramref name="value" />.
+    /// </returns>
+    public static TimeSpan Seconds(this ushort value)
+    {
+        return TimeSpan.FromSeconds(value);
+    }
+
+    /// <summary>
+    ///     Returns a <see cref="TimeSpan" /> that represents this value as the number of minutes.
+    /// </summary>
+    /// <param name="value">The duration, in minutes.</param>
+    /// <returns>
+    ///     A <see cref="TimeSpan" /> whose <see cref="TimeSpan.TotalMinutes" /> will equal <paramref name="value" />.
+    /// </returns>
+    public static TimeSpan Minutes(this ushort value)
+    {
+        return TimeSpan.FromMinutes(value);
+    }
+
+    /// <summary>
+    ///     Returns a <see cref="TimeSpan" /> that represents this value as the number of hours.
+    /// </summary>
+    /// <param name="value">The duration, in hours.</param>
+    /// <returns>
+    ///     A <see cref="TimeSpan" /> whose <see cref="TimeSpan.TotalHours" /> will equal <paramref name="value" />.
+    /// </returns>
+    public static TimeSpan Hours(this ushort value)
+    {
+        return TimeSpan.FromHours(value);
+    }
+
+    /// <summary>
+    ///     Returns a <see cref="TimeSpan" /> that represents this value as the number of days.
+    /// </summary>
+    /// <param name="value">The duration, in days.</param>
+    /// <returns>A <see cref="TimeSpan" /> whose <see cref="TimeSpan.TotalDays" /> will equal <paramref name="value" />.</returns>
+    public static TimeSpan Days(this ushort value)
+    {
+        return TimeSpan.FromDays(value);
+    }
+
+    /// <summary>
+    ///     Returns a <see cref="TimeSpan" /> that represents this value as the number of weeks.
+    /// </summary>
+    /// <param name="value">The duration, in weeks.</param>
+    /// <returns>
+    ///     A <see cref="TimeSpan" /> whose <see cref="TimeSpan.TotalDays" /> will equal <paramref name="value" /> × 7.
+    /// </returns>
+    public static TimeSpan Weeks(this ushort value)
+    {
+        return TimeSpan.FromDays(value * 7);
+    }
+}
---
+++ X10D/src/Time/UInt64Extensions.cs
@@ -0,0 +1,143 @@
+﻿namespace X10D.Time;
+
+/// <summary>
+///     Time-related extension methods for <see cref="ulong" />.
+/// </summary>
+[CLSCompliant(false)]
+public static class UInt64Extensions
+{
+    /// <summary>
+    ///     Returns a <see cref="TimeSpan" /> that represents this value as the number of ticks.
+    /// </summary>
+    /// <param name="value">The duration, in ticks.</param>
+    /// <returns>A <see cref="TimeSpan" /> whose <see cref="TimeSpan.Ticks" /> will equal <paramref name="value" />.</returns>
+    public static TimeSpan Ticks(this ulong value)
+    {
+        long remainder = 0;
+        if (value > long.MaxValue)
+        {
+            remainder = (long)(value - long.MaxValue);
+            value -= long.MaxValue;
+        }
+
+        return TimeSpan.FromTicks((long)value).Add(TimeSpan.FromTicks(remainder));
+    }
+
+    /// <summary>
+    ///     Returns a <see cref="TimeSpan" /> that represents this value as the number of milliseconds.
+    /// </summary>
+    /// <param name="value">The duration, in milliseconds.</param>
+    /// <returns>
+    ///     A <see cref="TimeSpan" /> whose <see cref="TimeSpan.TotalMilliseconds" /> will equal <paramref name="value" />.
+    /// </returns>
+    public static TimeSpan Milliseconds(this ulong value)
+    {
+        long remainder = 0;
+
+        if (value > long.MaxValue)
+        {
+            remainder = (long)(value - long.MaxValue);
+            value -= long.MaxValue;
+        }
+
+        return TimeSpan.FromMilliseconds((long)value).Add(TimeSpan.FromMilliseconds(remainder));
+    }
+
+    /// <summary>
+    ///     Returns a <see cref="TimeSpan" /> that represents this value as the number of seconds.
+    /// </summary>
+    /// <param name="value">The duration, in seconds.</param>
+    /// <returns>
+    ///     A <see cref="TimeSpan" /> whose <see cref="TimeSpan.TotalSeconds" /> will equal <paramref name="value" />.
+    /// </returns>
+    public static TimeSpan Seconds(this ulong value)
+    {
+        long remainder = 0;
+
+        if (value > long.MaxValue)
+        {
+            remainder = (long)(value - long.MaxValue);
+            value -= long.MaxValue;
+        }
+
+        return TimeSpan.FromSeconds((long)value).Add(TimeSpan.FromSeconds(remainder));
+    }
+
+    /// <summary>
+    ///     Returns a <see cref="TimeSpan" /> that represents this value as the number of minutes.
+    /// </summary>
+    /// <param name="value">The duration, in minutes.</param>
+    /// <returns>
+    ///     A <see cref="TimeSpan" /> whose <see cref="TimeSpan.TotalMinutes" /> will equal <paramref name="value" />.
+    /// </returns>
+    public static TimeSpan Minutes(this ulong value)
+    {
+        long remainder = 0;
+
+        if (value > long.MaxValue)
+        {
+            remainder = (long)(value - long.MaxValue);
+            value -= long.MaxValue;
+        }
+
+        return TimeSpan.FromMinutes((long)value).Add(TimeSpan.FromMinutes(remainder));
+    }
+
+    /// <summary>
+    ///     Returns a <see cref="TimeSpan" /> that represents this value as the number of hours.
+    /// </summary>
+    /// <param name="value">The duration, in hours.</param>
+    /// <returns>
+    ///     A <see cref="TimeSpan" /> whose <see cref="TimeSpan.TotalHours" /> will equal <paramref name="value" />.
+    /// </returns>
+    public static TimeSpan Hours(this ulong value)
+    {
+        long remainder = 0;
+
+        if (value > long.MaxValue)
+        {
+            remainder = (long)(value - long.MaxValue);
+            value -= long.MaxValue;
+        }
+
+        return TimeSpan.FromHours((long)value).Add(TimeSpan.FromHours(remainder));
+    }
+
+    /// <summary>
+    ///     Returns a <see cref="TimeSpan" /> that represents this value as the number of days.
+    /// </summary>
+    /// <param name="value">The duration, in days.</param>
+    /// <returns>A <see cref="TimeSpan" /> whose <see cref="TimeSpan.TotalDays" /> will equal <paramref name="value" />.</returns>
+    public static TimeSpan Days(this ulong value)
+    {
+        long remainder = 0;
+
+        if (value > long.MaxValue)
+        {
+            remainder = (long)(value - long.MaxValue);
+            value -= long.MaxValue;
+        }
+
+        return TimeSpan.FromDays((long)value).Add(TimeSpan.FromDays(remainder));
+    }
+
+    /// <summary>
+    ///     Returns a <see cref="TimeSpan" /> that represents this value as the number of weeks.
+    /// </summary>
+    /// <param name="value">The duration, in weeks.</param>
+    /// <returns>
+    ///     A <see cref="TimeSpan" /> whose <see cref="TimeSpan.TotalDays" /> will equal <paramref name="value" /> × 7.
+    /// </returns>
+    public static TimeSpan Weeks(this ulong value)
+    {
+        long remainder = 0;
+
+        if (value > long.MaxValue)
+        {
+            remainder = (long)(value - long.MaxValue);
+            value -= long.MaxValue;
+        }
+
+        return TimeSpan.FromDays((long)value * 7).Add(TimeSpan.FromDays(remainder * 7));
+    }
+}
