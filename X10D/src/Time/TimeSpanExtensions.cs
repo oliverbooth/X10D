a1722e4374ea6dacac8c94b0c725b386979644c8
Oliver Booth <me@olivr.me>
2022-04-21 20:54:17 +0100

    Add time-related extension methods

---
+++ X10D/src/Time/TimeSpanExtensions.cs
@@ -0,0 +1,31 @@
+﻿namespace X10D.Time;
+
+/// <summary>
+///     Extension methods for <see cref="TimeSpan" />.
+/// </summary>
+public static class TimeSpanExtensions
+{
+    /// <summary>
+    ///     Returns a <see cref="DateTime" /> that is a specified duration in the past relative to the current time.
+    /// </summary>
+    /// <param name="value">The <see cref="TimeSpan" /> whose duration to subtract.</param>
+    /// <returns>
+    ///     A <see cref="DateTime" /> that is a duration of <paramref name="value" /> in the past relative to the current time.
+    /// </returns>
+    public static DateTime Ago(this TimeSpan value)
+    {
+        return DateTime.Now.Subtract(value);
+    }
+
+    /// <summary>
+    ///     Returns a <see cref="DateTime" /> that is a specified duration in the future relative to the current time.
+    /// </summary>
+    /// <param name="value">The <see cref="TimeSpan" /> whose duration to add.</param>
+    /// <returns>
+    ///     A <see cref="DateTime" /> that is a duration of <paramref name="value" /> in the future relative to the current time.
+    /// </returns>
+    public static DateTime FromNow(this TimeSpan value)
+    {
+        return DateTime.Now.Add(value);
+    }
+}
