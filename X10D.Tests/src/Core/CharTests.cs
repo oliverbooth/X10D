748ff65221eb334eaa8aad22b76969aea61538a4
Oliver Booth <me@olivr.me>
2022-04-21 12:15:29 +0100

    [ci skip] Use file-scoped namespaces for tests

--- X10D.Tests/src/Core/CharTests.cs
+++ X10D.Tests/src/Core/CharTests.cs
@@ -1,24 +1,23 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace X10D.Tests.Core
+namespace X10D.Tests.Core;
+
+/// <summary>
+///     Tests for <see cref="CharExtensions" />.
+/// </summary>
+[TestClass]
+public class CharTests
 {
-    /// <summary>
-    ///     Tests for <see cref="CharExtensions" />.
-    /// </summary>
-    [TestClass]
-    public class CharTests
+    [TestMethod]
+    public void Repeat()
     {
-        [TestMethod]
-        public void Repeat()
-        {
-            const char character = 'a';
-            const int repeatCount = 10;
+        const char character = 'a';
+        const int repeatCount = 10;
 
-            const string repeated = "aaaaaaaaaa";
-            string result = character.Repeat(repeatCount);
+        const string repeated = "aaaaaaaaaa";
+        string result = character.Repeat(repeatCount);
 
-            Assert.AreEqual(repeated, result);
-            Assert.ThrowsException<ArgumentOutOfRangeException>(() => character.Repeat(-1));
-        }
+        Assert.AreEqual(repeated, result);
+        Assert.ThrowsException<ArgumentOutOfRangeException>(() => character.Repeat(-1));
     }
 }
--- X10D.Tests/src/Core/StringTests.cs
+++ X10D.Tests/src/Core/StringTests.cs
@@ -1,236 +1,235 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace X10D.Tests.Core
+namespace X10D.Tests.Core;
+
+/// <summary>
+///     Tests for <see cref="StringExtensions" />.
+/// </summary>
+[TestClass]
+public class StringTests
 {
     /// <summary>
-    ///     Tests for <see cref="StringExtensions" />.
+    ///     Tests <see cref="StringExtensions.AsNullIfEmpty" />.
     /// </summary>
-    [TestClass]
-    public class StringTests
+    [TestMethod]
+    public void AsNullIfEmpty()
     {
-        /// <summary>
-        ///     Tests <see cref="StringExtensions.AsNullIfEmpty" />.
-        /// </summary>
-        [TestMethod]
-        public void AsNullIfEmpty()
-        {
-            const string sampleString = "Hello World";
-            const string whitespaceString = " ";
-            const string emptyString = "";
-            const string? nullString = null;
+        const string sampleString = "Hello World";
+        const string whitespaceString = " ";
+        const string emptyString = "";
+        const string? nullString = null;
 
-            string sampleResult = sampleString.AsNullIfEmpty();
-            string whitespaceResult = whitespaceString.AsNullIfEmpty();
-            string emptyResult = emptyString.AsNullIfEmpty();
-            string? nullResult = nullString.AsNullIfEmpty();
+        string sampleResult = sampleString.AsNullIfEmpty();
+        string whitespaceResult = whitespaceString.AsNullIfEmpty();
+        string emptyResult = emptyString.AsNullIfEmpty();
+        string? nullResult = nullString.AsNullIfEmpty();
 
-            Assert.AreEqual(sampleString, sampleResult);
-            Assert.AreEqual(whitespaceString, whitespaceResult);
-            Assert.AreEqual(nullString, emptyResult);
-            Assert.AreEqual(nullString, nullResult);
-        }
+        Assert.AreEqual(sampleString, sampleResult);
+        Assert.AreEqual(whitespaceString, whitespaceResult);
+        Assert.AreEqual(nullString, emptyResult);
+        Assert.AreEqual(nullString, nullResult);
+    }
 
-        /// <summary>
-        ///     Tests <see cref="StringExtensions.AsNullIfWhiteSpace" />.
-        /// </summary>
-        [TestMethod]
-        public void AsNullIfWhiteSpace()
-        {
-            const string sampleString = "Hello World";
-            const string whitespaceString = " ";
-            const string emptyString = "";
-            const string? nullString = null;
+    /// <summary>
+    ///     Tests <see cref="StringExtensions.AsNullIfWhiteSpace" />.
+    /// </summary>
+    [TestMethod]
+    public void AsNullIfWhiteSpace()
+    {
+        const string sampleString = "Hello World";
+        const string whitespaceString = " ";
+        const string emptyString = "";
+        const string? nullString = null;
 
-            string sampleResult = sampleString.AsNullIfWhiteSpace();
-            string whitespaceResult = whitespaceString.AsNullIfWhiteSpace();
-            string emptyResult = emptyString.AsNullIfWhiteSpace();
-            string? nullResult = nullString.AsNullIfWhiteSpace();
+        string sampleResult = sampleString.AsNullIfWhiteSpace();
+        string whitespaceResult = whitespaceString.AsNullIfWhiteSpace();
+        string emptyResult = emptyString.AsNullIfWhiteSpace();
+        string? nullResult = nullString.AsNullIfWhiteSpace();
 
-            Assert.AreEqual(sampleString, sampleResult);
-            Assert.AreEqual(nullString, whitespaceResult);
-            Assert.AreEqual(nullString, emptyResult);
-            Assert.AreEqual(nullString, nullResult);
-        }
+        Assert.AreEqual(sampleString, sampleResult);
+        Assert.AreEqual(nullString, whitespaceResult);
+        Assert.AreEqual(nullString, emptyResult);
+        Assert.AreEqual(nullString, nullResult);
+    }
 
-        /// <summary>
-        ///     Tests <see cref="StringExtensions.Base64Decode" />.
-        /// </summary>
-        [TestMethod]
-        public void Base64Decode()
-        {
-            const string input = "SGVsbG8gV29ybGQ=";
-            const string expected = "Hello World";
+    /// <summary>
+    ///     Tests <see cref="StringExtensions.Base64Decode" />.
+    /// </summary>
+    [TestMethod]
+    public void Base64Decode()
+    {
+        const string input = "SGVsbG8gV29ybGQ=";
+        const string expected = "Hello World";
 
-            string result = input.Base64Decode();
+        string result = input.Base64Decode();
 
-            Assert.AreEqual(expected, result);
-        }
+        Assert.AreEqual(expected, result);
+    }
 
 
-        /// <summary>
-        ///     Tests <see cref="StringExtensions.Base64Encode" />.
-        /// </summary>
-        [TestMethod]
-        public void Base64Encode()
-        {
-            const string input = "Hello World";
-            const string expected = "SGVsbG8gV29ybGQ=";
+    /// <summary>
+    ///     Tests <see cref="StringExtensions.Base64Encode" />.
+    /// </summary>
+    [TestMethod]
+    public void Base64Encode()
+    {
+        const string input = "Hello World";
+        const string expected = "SGVsbG8gV29ybGQ=";
 
-            string result = input.Base64Encode();
+        string result = input.Base64Encode();
 
-            Assert.AreEqual(expected, result);
-        }
+        Assert.AreEqual(expected, result);
+    }
 
-        /// <summary>
-        ///     Tests <see cref="StringExtensions.IsLower" />.
-        /// </summary>
-        [TestMethod]
-        public void IsLower()
-        {
-            const string inputA = "Hello World";
-            const string inputB = "hello world";
-            const string? nullString = null;
+    /// <summary>
+    ///     Tests <see cref="StringExtensions.IsLower" />.
+    /// </summary>
+    [TestMethod]
+    public void IsLower()
+    {
+        const string inputA = "Hello World";
+        const string inputB = "hello world";
+        const string? nullString = null;
 
-            bool resultA = inputA.IsLower();
-            bool resultB = inputB.IsLower();
+        bool resultA = inputA.IsLower();
+        bool resultB = inputB.IsLower();
 
-            Assert.ThrowsException<ArgumentNullException>(() => nullString!.IsLower());
-            Assert.IsFalse(resultA);
-            Assert.IsTrue(resultB);
-        }
+        Assert.ThrowsException<ArgumentNullException>(() => nullString!.IsLower());
+        Assert.IsFalse(resultA);
+        Assert.IsTrue(resultB);
+    }
 
-        [TestMethod]
-        public void IsPalindrome()
-        {
-            const string inputA = "Race car";
-            const string inputB = "Racecar";
-            const string inputC = "A man, a plan, a canal, panama";
-            const string inputD = "Jackdaws love my big sphinx of quartz";
-            const string inputE = "Y";
-            const string inputF = "1";
-            const string inputG = "";
+    [TestMethod]
+    public void IsPalindrome()
+    {
+        const string inputA = "Race car";
+        const string inputB = "Racecar";
+        const string inputC = "A man, a plan, a canal, panama";
+        const string inputD = "Jackdaws love my big sphinx of quartz";
+        const string inputE = "Y";
+        const string inputF = "1";
+        const string inputG = "";
 
-            Assert.IsTrue(inputA.IsPalindrome(), inputA);
-            Assert.IsTrue(inputB.IsPalindrome(), inputB);
-            Assert.IsTrue(inputC.IsPalindrome(), inputC);
-            Assert.IsFalse(inputD.IsPalindrome(), inputD);
-            Assert.IsTrue(inputE.IsPalindrome(), inputE);
-            Assert.IsTrue(inputF.IsPalindrome(), inputF);
-            Assert.IsFalse(inputG.IsPalindrome(), inputG);
-        }
+        Assert.IsTrue(inputA.IsPalindrome(), inputA);
+        Assert.IsTrue(inputB.IsPalindrome(), inputB);
+        Assert.IsTrue(inputC.IsPalindrome(), inputC);
+        Assert.IsFalse(inputD.IsPalindrome(), inputD);
+        Assert.IsTrue(inputE.IsPalindrome(), inputE);
+        Assert.IsTrue(inputF.IsPalindrome(), inputF);
+        Assert.IsFalse(inputG.IsPalindrome(), inputG);
+    }
 
-        /// <summary>
-        ///     Tests <see cref="StringExtensions.IsUpper" />.
-        /// </summary>
-        [TestMethod]
-        public void IsUpper()
-        {
-            const string inputA = "Hello World";
-            const string inputB = "HELLO WORLD";
-            const string? nullString = null;
+    /// <summary>
+    ///     Tests <see cref="StringExtensions.IsUpper" />.
+    /// </summary>
+    [TestMethod]
+    public void IsUpper()
+    {
+        const string inputA = "Hello World";
+        const string inputB = "HELLO WORLD";
+        const string? nullString = null;
 
-            bool resultA = inputA.IsUpper();
-            bool resultB = inputB.IsUpper();
+        bool resultA = inputA.IsUpper();
+        bool resultB = inputB.IsUpper();
 
-            Assert.ThrowsException<ArgumentNullException>(() => nullString!.IsUpper());
-            Assert.IsFalse(resultA);
-            Assert.IsTrue(resultB);
-        }
+        Assert.ThrowsException<ArgumentNullException>(() => nullString!.IsUpper());
+        Assert.IsFalse(resultA);
+        Assert.IsTrue(resultB);
+    }
 
-        /// <summary>
-        ///     Tests <see cref="StringExtensions.Shuffled(string, Random)" />.
-        /// </summary>
-        [TestMethod]
-        public void Shuffled()
-        {
-            const string input = "Hello World";
-            const string expected = " oHlldrWoel";
-            var random = new Random(1);
+    /// <summary>
+    ///     Tests <see cref="StringExtensions.Shuffled(string, Random)" />.
+    /// </summary>
+    [TestMethod]
+    public void Shuffled()
+    {
+        const string input = "Hello World";
+        const string expected = " oHlldrWoel";
+        var random = new Random(1);
 
-            string result = input.Shuffled(random);
+        string result = input.Shuffled(random);
 
-            Assert.ThrowsException<ArgumentNullException>(() => ((string?)null)!.Shuffled());
-            Assert.AreEqual(expected, result);
-        }
+        Assert.ThrowsException<ArgumentNullException>(() => ((string?)null)!.Shuffled());
+        Assert.AreEqual(expected, result);
+    }
 
-        /// <summary>
-        ///     Tests <see cref="StringExtensions.Randomize" />.
-        /// </summary>
-        [TestMethod]
-        public void Randomize()
-        {
-            const string input = "Hello World";
-            const string expected = "le rooldeoH";
-            var random = new Random(1);
+    /// <summary>
+    ///     Tests <see cref="StringExtensions.Randomize" />.
+    /// </summary>
+    [TestMethod]
+    public void Randomize()
+    {
+        const string input = "Hello World";
+        const string expected = "le rooldeoH";
+        var random = new Random(1);
 
-            string result = input.Randomize(input.Length, random);
+        string result = input.Randomize(input.Length, random);
 
-            Assert.ThrowsException<ArgumentNullException>(() => ((string?)null)!.Randomize(1));
-            Assert.ThrowsException<ArgumentOutOfRangeException>(() => input.Randomize(-1));
-            Assert.AreEqual(string.Empty, string.Empty.Randomize(0));
-            Assert.AreEqual(expected, result);
-        }
+        Assert.ThrowsException<ArgumentNullException>(() => ((string?)null)!.Randomize(1));
+        Assert.ThrowsException<ArgumentOutOfRangeException>(() => input.Randomize(-1));
+        Assert.AreEqual(string.Empty, string.Empty.Randomize(0));
+        Assert.AreEqual(expected, result);
+    }
 
-        /// <summary>
-        ///     Tests <see cref="StringExtensions.Repeat" />.
-        /// </summary>
-        [TestMethod]
-        public void Repeat()
-        {
-            const string input = "Hello World";
-            const string expected = "Hello WorldHello WorldHello WorldHello WorldHello WorldHello WorldHello WorldHello World";
-            const int repeatCount = 8;
+    /// <summary>
+    ///     Tests <see cref="StringExtensions.Repeat" />.
+    /// </summary>
+    [TestMethod]
+    public void Repeat()
+    {
+        const string input = "Hello World";
+        const string expected = "Hello WorldHello WorldHello WorldHello WorldHello WorldHello WorldHello WorldHello World";
+        const int repeatCount = 8;
 
-            string result = input.Repeat(repeatCount);
+        string result = input.Repeat(repeatCount);
 
-            Assert.ThrowsException<ArgumentNullException>(() => ((string?)null)!.Repeat(repeatCount));
-            Assert.ThrowsException<ArgumentOutOfRangeException>(() => input.Repeat(-1));
-            Assert.AreEqual(expected, result);
-        }
+        Assert.ThrowsException<ArgumentNullException>(() => ((string?)null)!.Repeat(repeatCount));
+        Assert.ThrowsException<ArgumentOutOfRangeException>(() => input.Repeat(-1));
+        Assert.AreEqual(expected, result);
+    }
 
-        /// <summary>
-        ///     Tests <see cref="StringExtensions.Repeat" />.
-        /// </summary>
-        [TestMethod]
-        public void Reverse()
-        {
-            const string input = "Hello World";
-            const string expected = "dlroW olleH";
+    /// <summary>
+    ///     Tests <see cref="StringExtensions.Repeat" />.
+    /// </summary>
+    [TestMethod]
+    public void Reverse()
+    {
+        const string input = "Hello World";
+        const string expected = "dlroW olleH";
 
-            string result = input.Reverse();
+        string result = input.Reverse();
 
-            Assert.ThrowsException<ArgumentNullException>(() => ((string?)null)!.Reverse());
-            Assert.AreEqual(string.Empty.Reverse(), string.Empty);
-            Assert.AreEqual(" ".Reverse(), " ");
-            Assert.AreEqual(expected, result);
-        }
+        Assert.ThrowsException<ArgumentNullException>(() => ((string?)null)!.Reverse());
+        Assert.AreEqual(string.Empty.Reverse(), string.Empty);
+        Assert.AreEqual(" ".Reverse(), " ");
+        Assert.AreEqual(expected, result);
+    }
 
-        /// <summary>
-        ///     Tests <see cref="StringExtensions.WithEmptyAlternative" /> and
-        ///     <see cref="StringExtensions.WithWhiteSpaceAlternative"/>.
-        /// </summary>
-        [TestMethod]
-        public void WithAlternative()
-        {
-            const string inputA = "Hello World";
-            const string inputB = " ";
-            const string inputC = "";
-            const string? inputD = null;
-            const string alternative = "ALTERNATIVE";
+    /// <summary>
+    ///     Tests <see cref="StringExtensions.WithEmptyAlternative" /> and
+    ///     <see cref="StringExtensions.WithWhiteSpaceAlternative"/>.
+    /// </summary>
+    [TestMethod]
+    public void WithAlternative()
+    {
+        const string inputA = "Hello World";
+        const string inputB = " ";
+        const string inputC = "";
+        const string? inputD = null;
+        const string alternative = "ALTERNATIVE";
 
-            string resultA = inputA.WithEmptyAlternative(alternative);
-            string resultB = inputB.WithEmptyAlternative(alternative);
-            string resultBWithWhitespace = inputB.WithWhiteSpaceAlternative(alternative);
-            string resultC = inputC.WithEmptyAlternative(alternative);
-            string resultD = inputD.WithEmptyAlternative(alternative);
+        string resultA = inputA.WithEmptyAlternative(alternative);
+        string resultB = inputB.WithEmptyAlternative(alternative);
+        string resultBWithWhitespace = inputB.WithWhiteSpaceAlternative(alternative);
+        string resultC = inputC.WithEmptyAlternative(alternative);
+        string resultD = inputD.WithEmptyAlternative(alternative);
 
-            Assert.AreEqual(resultA, inputA);
-            Assert.AreEqual(resultB, inputB);
-            Assert.AreEqual(resultBWithWhitespace, alternative);
-            Assert.AreEqual(resultC, alternative);
-            Assert.AreEqual(resultD, alternative);
-            Assert.AreEqual(alternative, ((string?)null).WithEmptyAlternative(alternative));
-            Assert.AreEqual(alternative, ((string?)null).WithWhiteSpaceAlternative(alternative));
-        }
+        Assert.AreEqual(resultA, inputA);
+        Assert.AreEqual(resultB, inputB);
+        Assert.AreEqual(resultBWithWhitespace, alternative);
+        Assert.AreEqual(resultC, alternative);
+        Assert.AreEqual(resultD, alternative);
+        Assert.AreEqual(alternative, ((string?)null).WithEmptyAlternative(alternative));
+        Assert.AreEqual(alternative, ((string?)null).WithWhiteSpaceAlternative(alternative));
     }
 }
