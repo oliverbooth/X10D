0f27f5956f2e7a20d2e7a01fe6c57c315a4726ac
Oliver Booth <me@olivr.me>
2022-04-21 21:08:23 +0100

    Add tests for Net.EndPointExtensions

---
+++ X10D.Tests/src/Net/EndPointTests.cs
@@ -0,0 +1,44 @@
+﻿using System.Net;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using X10D.Net;
+
+namespace X10D.Tests.Net;
+
+[TestClass]
+public class EndPointTests
+{
+    [TestMethod]
+    public void DnsEndPoint_GetHost_Localhost()
+    {
+        var endPoint = new DnsEndPoint("localhost", 1234);
+        Assert.AreEqual("localhost", endPoint.GetHost());
+    }
+    
+    [TestMethod]
+    public void DnsEndPoint_GetPort_1234()
+    {
+        var endPoint = new DnsEndPoint("localhost", 1234);
+        Assert.AreEqual(1234, endPoint.GetPort());
+    }
+    
+    [TestMethod]
+    public void IPEndPoint_IPv4_Loopback_GetHost_127_0_0_1()
+    {
+        var endPoint = new IPEndPoint(IPAddress.Loopback, 1234);
+        Assert.AreEqual("127.0.0.1", endPoint.GetHost());
+    }
+    
+    [TestMethod]
+    public void IPEndPoint_IPv6_Loopback_GetHost_ColonColon1()
+    {
+        var endPoint = new IPEndPoint(IPAddress.IPv6Loopback, 1234);
+        Assert.AreEqual("::1", endPoint.GetHost());
+    }
+    
+    [TestMethod]
+    public void IPEndPoint_GetPort_1234()
+    {
+        var endPoint = new IPEndPoint(IPAddress.Loopback, 1234);
+        Assert.AreEqual("127.0.0.1", endPoint.GetHost());
+    }
+}
