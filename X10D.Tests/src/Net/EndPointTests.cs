e421b357fded7650035c596bd786913920997b23
Oliver Booth <me@olivr.me>
2022-04-21 21:44:24 +0100

    [ci skip] Add IpAddressTests

--- X10D.Tests/src/Net/EndPointTests.cs
+++ X10D.Tests/src/Net/EndPointTests.cs
@@ -13,42 +13,42 @@ public class EndPointTests
         Assert.ThrowsException<ArgumentNullException>(() => ((IPEndPoint?)null)!.GetHost());
         Assert.ThrowsException<ArgumentNullException>(() => ((DnsEndPoint?)null)!.GetHost());
     }
-    
+
     [TestMethod]
     public void GetPort_Null_ShouldThrow()
     {
         Assert.ThrowsException<ArgumentNullException>(() => ((IPEndPoint?)null)!.GetPort());
         Assert.ThrowsException<ArgumentNullException>(() => ((DnsEndPoint?)null)!.GetPort());
     }
-    
+
     [TestMethod]
     public void DnsEndPoint_GetHost_Localhost()
     {
         var endPoint = new DnsEndPoint("localhost", 1234);
         Assert.AreEqual("localhost", endPoint.GetHost());
     }
-    
+
     [TestMethod]
     public void DnsEndPoint_GetPort_1234()
     {
         var endPoint = new DnsEndPoint("localhost", 1234);
         Assert.AreEqual(1234, endPoint.GetPort());
     }
-    
+
     [TestMethod]
     public void IPEndPoint_IPv4_Loopback_GetHost_127_0_0_1()
     {
         var endPoint = new IPEndPoint(IPAddress.Loopback, 1234);
         Assert.AreEqual("127.0.0.1", endPoint.GetHost());
     }
-    
+
     [TestMethod]
     public void IPEndPoint_IPv6_Loopback_GetHost_ColonColon1()
     {
         var endPoint = new IPEndPoint(IPAddress.IPv6Loopback, 1234);
         Assert.AreEqual("::1", endPoint.GetHost());
     }
-    
+
     [TestMethod]
     public void IPEndPoint_GetPort_1234()
     {
