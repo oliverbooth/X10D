cfa27e0b4fb181ba948a9e6ce79e3774a80a55f9
Oliver Booth <me@olivr.me>
2022-04-22 12:16:34 +0100

    Move GetHash to child namespace (#7)
    Also introduces TryWriteHash

---
+++ X10D.Tests/src/IO/StreamTests.cs
@@ -0,0 +1,59 @@
+﻿using System.Diagnostics;
+using System.Security.Cryptography;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using X10D.IO;
+
+namespace X10D.Tests.IO;
+
+[TestClass]
+public class StreamTests
+{
+    [TestMethod]
+    public void GetHashSha1ShouldBeCorrect()
+    {
+        // SHA-1
+        byte[] expectedHash =
+        {
+            0x0A, 0x4D, 0x55, 0xA8, 0xD7, 0x78, 0xE5, 0x02, 0x2F, 0xAB, 0x70, 0x19, 0x77, 0xC5, 0xD8, 0x40, 0xBB, 0xC4, 0x86,
+            0xD0
+        };
+
+        using var stream = new MemoryStream();
+        stream.Write(Encoding.UTF8.GetBytes("Hello World"));
+        stream.Position = 0;
+
+        byte[] hash = stream.GetHash<SHA1>();
+        Trace.WriteLine($"Hash: {BitConverter.ToString(hash)}");
+        Trace.WriteLine($"Expected: {BitConverter.ToString(expectedHash)}");
+        CollectionAssert.AreEqual(expectedHash, hash);
+    }
+
+    [TestMethod]
+    public void GetHashNullShouldThrow()
+    {
+        // any HashAlgorithm will do, but SHA1 is used above. so to remain consistent, we use it here
+        Assert.ThrowsException<ArgumentNullException>(() => ((Stream?)null)!.GetHash<SHA1>());
+        Assert.ThrowsException<ArgumentNullException>(() => ((Stream?)null)!.TryWriteHash<SHA1>(Span<byte>.Empty, out _));
+    }
+
+    [TestMethod]
+    public void TryWriteHashSha1ShouldBeCorrect()
+    {
+        // SHA-1
+        byte[] expectedHash =
+        {
+            0x0A, 0x4D, 0x55, 0xA8, 0xD7, 0x78, 0xE5, 0x02, 0x2F, 0xAB, 0x70, 0x19, 0x77, 0xC5, 0xD8, 0x40, 0xBB, 0xC4, 0x86,
+            0xD0
+        };
+
+        using var stream = new MemoryStream();
+        stream.Write(Encoding.UTF8.GetBytes("Hello World"));
+        stream.Position = 0;
+
+        Span<byte> hash = stackalloc byte[20];
+        stream.TryWriteHash<SHA1>(hash, out int bytesWritten);
+        Assert.AreEqual(expectedHash.Length, bytesWritten);
+        CollectionAssert.AreEqual(expectedHash, hash.ToArray());
+    }
+}
---
+++ X10D/src/IO/FileInfoExtensions.cs
@@ -0,0 +1,74 @@
+﻿using System.Security.Cryptography;
+
+namespace X10D.IO;
+
+/// <summary>
+///     IO-related extension methods for <see cref="FileInfo" />.
+/// </summary>
+public static class FileInfoExtensions
+{
+    /// <summary>
+    ///     Computes the hash of a file using the specified hash algorithm.
+    /// </summary>
+    /// <param name="value">The file whose hash to compute.</param>
+    /// <typeparam name="T">
+    ///     The type of the <see cref="HashAlgorithm" /> whose <see cref="HashAlgorithm.ComputeHash(Stream)" /> is to be used for
+    ///     computing the hash.
+    /// </typeparam>
+    /// <returns>The hash of <paramref name="stream" /> represented as an array of bytes.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="value" /> is <see langword="null" />.</exception>
+    /// <exception cref="FileNotFoundException">The specified file was not found.</exception>
+    /// <exception cref="IOException">The opened file stream cannot be read.</exception>
+    /// <exception cref="TypeInitializationException">
+    ///     The specified <see cref="HashAlgorithm" /> does not offer a public, static. parameterless <c>Create</c> method, or its
+    ///     <c>Create</c> method returns a type that is not assignable to <typeparamref name="T" />.
+    /// </exception>
+    /// <exception cref="ObjectDisposedException">The stream has already been disposed.</exception>
+    public static byte[] GetHash<T>(this FileInfo value)
+        where T : HashAlgorithm
+    {
+        if (value is null)
+        {
+            throw new ArgumentNullException(nameof(value));
+        }
+
+        using FileStream stream = value.OpenRead();
+        return stream.GetHash<T>();
+    }
+
+    /// <summary>
+    ///     Computes the hash of a file using the specified hash algorithm.
+    /// </summary>
+    /// <param name="value">The file whose hash to compute.</param>
+    /// <param name="destination">When this method returns, contains the computed hash of <paramref name="value" />.</param>
+    /// <param name="bytesWritten">
+    ///     When this method returns, the total number of bytes written into destination. This parameter is treated as
+    ///     uninitialized.
+    /// </param>
+    /// <typeparam name="T">
+    ///     The type of the <see cref="HashAlgorithm" /> whose <see cref="HashAlgorithm.ComputeHash(Stream)" /> is to be used for
+    ///     computing the hash.
+    /// </typeparam>
+    /// <returns>
+    ///     <see langword="true" /> if the destination is long enough to receive the hash; otherwise, <see langword="false" />.
+    /// </returns>
+    /// <exception cref="ArgumentNullException"><paramref name="value" /> is <see langword="null" />.</exception>
+    /// <exception cref="FileNotFoundException">The specified file was not found.</exception>
+    /// <exception cref="IOException">The opened file stream cannot be read.</exception>
+    /// <exception cref="TypeInitializationException">
+    ///     The specified <see cref="HashAlgorithm" /> does not offer a public, static. parameterless <c>Create</c> method, or its
+    ///     <c>Create</c> method returns a type that is not assignable to <typeparamref name="T" />.
+    /// </exception>
+    /// <exception cref="ObjectDisposedException">The stream has already been disposed.</exception>
+    public static bool TryWriteHash<T>(this FileInfo value, Span<byte> destination, out int bytesWritten)
+        where T : HashAlgorithm
+    {
+        if (value is null)
+        {
+            throw new ArgumentNullException(nameof(value));
+        }
+
+        using FileStream stream = value.OpenRead();
+        return stream.TryWriteHash<T>(destination, out bytesWritten);
+    }
+}
