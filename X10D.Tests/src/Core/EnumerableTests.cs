a35d93bdf8a33fc192fa2b7092890f22581018a3
Oliver Booth <me@olivr.me>
2022-04-24 10:29:04 +0100

    Fix nullability of AsEnumerable return type

--- X10D.Tests/src/Core/EnumerableTests.cs
+++ X10D.Tests/src/Core/EnumerableTests.cs
@@ -5,7 +5,7 @@ using X10D.Core;
 namespace X10D.Tests.Core;
 
 [TestClass]
-public partial class EnumerableTests
+public class EnumerableTests
 {
     [TestMethod]
     [DataRow(1)]
@@ -13,12 +13,12 @@ public partial class EnumerableTests
     [DataRow(true)]
     public void AsEnumerable(object o)
     {
-        IEnumerable<object> array = o.AsEnumerable().ToArray(); // prevent multiple enumeration of IEnumerable
+        IEnumerable<object?> array = o.AsEnumerable().ToArray(); // prevent multiple enumeration of IEnumerable
         Assert.IsNotNull(array);
         Assert.IsTrue(array.Count() == 1);
         Assert.AreEqual(o, array.ElementAt(0));
     }
-    
+
     [TestMethod]
     [DataRow(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)]
     public void Shuffled(params int[] source)
