c277b18bd2db6ddb5078c5ea935083db0d9dfb28
Oliver Booth <me@olivr.me>
2022-04-25 22:21:08 +0100

    Obsolesce WaitOneAsync
    TaskCompletionSource is recommended for execution suspension while waiting for an asynchronous operation to complete

--- X10D/src/Threading/WaitHandleExtensions.cs
+++ X10D/src/Threading/WaitHandleExtensions.cs
@@ -1,7 +1,7 @@
-﻿namespace X10D;
+﻿namespace X10D.Threading;
 
 /// <summary>
-///     Extension methods for <see cref="WaitHandle" />.
+///     Threading-related extension methods for <see cref="WaitHandle" />.
 /// </summary>
 public static class WaitHandleExtensions
 {
@@ -13,6 +13,12 @@ public static class WaitHandleExtensions
     ///     <see langword="true" /> if the current instance receives a signal. If the current instance is never signaled,
     ///     <see cref="WaitOneAsync" /> never returns.
     /// </returns>
+    /// <remarks>
+    ///     It is heavily recommended that the use of this method is minimal, or non-existent. For suspension of execution when
+    ///     performing an asynchronous operation, use <see cref="TaskCompletionSource" /> or
+    ///     <see cref="TaskCompletionSource{TResult}" />.
+    /// </remarks>
+    [Obsolete("Consider using a TaskCompletionSource instead.")]
     public static Task<bool> WaitOneAsync(this WaitHandle handle)
     {
         if (handle is null)
