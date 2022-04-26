f7f3ea71bbc57d13e39b745bff85444b0cb42a5b
Oliver Booth <me@olivr.me>
2022-04-26 10:45:18 +0100

    Append "Value" to AsArray/AsEnumerable, to reduce risk of LINQ collision

--- X10D.Tests/src/Core/CoreTests.cs
+++ X10D.Tests/src/Core/CoreTests.cs
@@ -9,18 +9,18 @@ public class CoreTests
     [TestMethod]
     public void AsArrayShouldBeLength1()
     {
-        Assert.AreEqual(1, 0.AsArray().Length);
-        Assert.AreEqual(1, string.Empty.AsArray().Length);
-        Assert.AreEqual(1, true.AsArray().Length);
-        Assert.AreEqual(1, ((object?)null).AsArray().Length);
+        Assert.AreEqual(1, 0.AsArrayValue().Length);
+        Assert.AreEqual(1, string.Empty.AsArrayValue().Length);
+        Assert.AreEqual(1, true.AsArrayValue().Length);
+        Assert.AreEqual(1, ((object?)null).AsArrayValue().Length);
     }
     
     [TestMethod]
     public void AsEnumerableShouldBeLength1()
     {
-        Assert.AreEqual(1, 0.AsEnumerable().Count());
-        Assert.AreEqual(1, string.Empty.AsEnumerable().Count());
-        Assert.AreEqual(1, true.AsEnumerable().Count());
-        Assert.AreEqual(1, ((object?)null).AsEnumerable().Count());
+        Assert.AreEqual(1, 0.AsEnumerableValue().Count());
+        Assert.AreEqual(1, string.Empty.AsEnumerableValue().Count());
+        Assert.AreEqual(1, true.AsEnumerableValue().Count());
+        Assert.AreEqual(1, ((object?)null).AsEnumerableValue().Count());
     }
 }
