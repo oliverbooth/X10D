b3dbfb88fd6fffdeee3c6e779f2eddbe5c5ef582
Oliver Booth <me@olivr.me>
2022-04-30 13:08:39 +0100

    Migrate string extensions to appropriate namespaces (#7)
    Introduces more tests

---
+++ X10D.Tests/src/Time/StringTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using X10D.Time;
+
+namespace X10D.Tests.Time;
+
+[TestClass]
+public class StringTests
+{
+    [TestMethod]
+    public void ToTimeSpan_ShouldReturnCorrectTimeSpan_GivenString()
+    {
+        const string value = "1y 1mo 1w 1d 1h 1m 1s 1ms";
+
+        TimeSpan expected = TimeSpan.FromMilliseconds(1);
+        expected += TimeSpan.FromSeconds(1);
+        expected += TimeSpan.FromMinutes(1);
+        expected += TimeSpan.FromHours(1);
+        expected += TimeSpan.FromDays(1);
+        expected += TimeSpan.FromDays(7);
+        expected += TimeSpan.FromDays(30);
+        expected += TimeSpan.FromDays(365);
+
+        Assert.AreEqual(expected, value.ToTimeSpan());
+    }
+
+    [TestMethod]
+    public void ToTimeSpan_ShouldReturnZero_GivenInvalidString()
+    {
+        Assert.AreEqual(TimeSpan.Zero, "Hello World".ToTimeSpan());
+    }
+
+    [TestMethod]
+    public void ToTimeSpan_ShouldThrow_GivenNullString()
+    {
+        string? value = null;
+        Assert.ThrowsException<ArgumentNullException>(() => value!.ToTimeSpan());
+    }
+}
