9d5ba982eb142018c2c635b11d7618d1459de2c2
Oliver Booth <me@olivr.me>
2022-05-08 19:29:59 +0100

    Add Color.Inverted (#54)

---
+++ X10D.Unity/src/Drawing/Color32Extensions.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics.Contracts;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+namespace X10D.Unity.Drawing;
+
+/// <summary>
+///     Drawing-related extensions for <see cref="Color32" />.
+/// </summary>
+public static class Color32Extensions
+{
+    /// <summary>
+    ///     Returns a new <see cref="Color32" /> with the red, green, and blue components inverted. Alpha is not affected.
+    /// </summary>
+    /// <param name="color">The color to invert.</param>
+    /// <returns>The inverted color.</returns>
+    [Pure]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Color32 Inverted(this Color32 color)
+    {
+        return new Color32((byte)(255 - color.r), (byte)(255 - color.g), (byte)(255 - color.b), color.a);
+    }
+}
---
+++ X10D.Unity/src/Drawing/ColorExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics.Contracts;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+namespace X10D.Unity.Drawing;
+
+/// <summary>
+///     Drawing-related extensions for <see cref="Color" />.
+/// </summary>
+public static class ColorExtensions
+{
+    /// <summary>
+    ///     Returns a new <see cref="Color" /> with the red, green, and blue components inverted. Alpha is not affected.
+    /// </summary>
+    /// <param name="color">The color to invert.</param>
+    /// <returns>The inverted color.</returns>
+    [Pure]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Color Inverted(this Color color)
+    {
+        return new Color(1f - color.r, 1f - color.g, 1f - color.b, color.a);
+    }
+}
