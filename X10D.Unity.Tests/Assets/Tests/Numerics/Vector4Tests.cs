48f4b2af68b11c564913c8513eedc4b4b9e04734
Oliver Booth <me@olivr.me>
2022-05-09 10:33:21 +0100

    Add Vector.With* methods (resolves #56)

---
+++ X10D.Unity.Tests/Assets/Tests/Numerics/Vector4Tests.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using X10D.Unity.Numerics;
+
+namespace X10D.Unity.Tests.Numerics
+{
+    public class Vector4Tests
+    {
+        [UnityTest]
+        public IEnumerator WithW_ShouldReturnVectorWithNewW_GivenVector()
+        {
+            Assert.AreEqual(new Vector4(1, 1, 1, 0), Vector4.one.WithW(0));
+            Assert.AreEqual(Vector4.zero, Vector4.zero.WithW(0));
+            Assert.AreEqual(Vector4.zero, new Vector4(0, 0, 0, 1).WithW(0));
+            Assert.AreEqual(new Vector4(1, 0, 0, 0), new Vector4(1, 0, 0, 0).WithW(0));
+            Assert.AreEqual(new Vector4(0, 1, 0, 0), new Vector4(0, 1, 0, 0).WithW(0));
+            Assert.AreEqual(new Vector4(0, 0, 1, 0), new Vector4(0, 0, 1, 0).WithW(0));
+
+            Assert.AreEqual(Vector4.one, Vector4.one.WithW(1));
+            Assert.AreEqual(new Vector4(0, 0, 0, 1), Vector4.zero.WithW(1));
+            Assert.AreEqual(new Vector4(0, 0, 0, 1), new Vector4(0, 0, 0, 1).WithW(1));
+            Assert.AreEqual(new Vector4(1, 0, 0, 1), new Vector4(1, 0, 0, 0).WithW(1));
+            Assert.AreEqual(new Vector4(0, 1, 0, 1), new Vector4(0, 1, 0, 0).WithW(1));
+            Assert.AreEqual(new Vector4(0, 0, 1, 1), new Vector4(0, 0, 1, 0).WithW(1));
+            
+            yield break;
+        }
+
+        [UnityTest]
+        public IEnumerator WithX_ShouldReturnVectorWithNewX_GivenVector()
+        {
+            Assert.AreEqual(new Vector4(0, 1, 1, 1), Vector4.one.WithX(0));
+            Assert.AreEqual(Vector4.zero, Vector4.zero.WithX(0));
+            Assert.AreEqual(new Vector4(0, 0, 0, 1), new Vector4(0, 0, 0, 1).WithX(0));
+            Assert.AreEqual(Vector4.zero, new Vector4(1, 0, 0, 0).WithX(0));
+            Assert.AreEqual(new Vector4(0, 1, 0, 0), new Vector4(0, 1, 0, 0).WithX(0));
+            Assert.AreEqual(new Vector4(0, 0, 1, 0), new Vector4(0, 0, 1, 0).WithX(0));
+
+            Assert.AreEqual(Vector4.one, Vector4.one.WithX(1));
+            Assert.AreEqual(new Vector4(1, 0, 0, 0), Vector4.zero.WithX(1));
+            Assert.AreEqual(new Vector4(1, 0, 0, 1), new Vector4(0, 0, 0, 1).WithX(1));
+            Assert.AreEqual(new Vector4(1, 0, 0, 0), new Vector4(1, 0, 0, 0).WithX(1));
+            Assert.AreEqual(new Vector4(1, 1, 0, 0), new Vector4(0, 1, 0, 0).WithX(1));
+            Assert.AreEqual(new Vector4(1, 0, 1, 0), new Vector4(0, 0, 1, 0).WithX(1));
+            
+            yield break;
+        }
+
+        [UnityTest]
+        public IEnumerator WithY_ShouldReturnVectorWithNewY_GivenVector()
+        {
+            Assert.AreEqual(new Vector4(1, 0, 1, 1), Vector4.one.WithY(0));
+            Assert.AreEqual(Vector4.zero, Vector4.zero.WithY(0));
+            Assert.AreEqual(new Vector4(0, 0, 0, 1), new Vector4(0, 0, 0, 1).WithY(0));
+            Assert.AreEqual(new Vector4(1, 0, 0, 0), new Vector4(1, 0, 0, 0).WithY(0));
+            Assert.AreEqual(Vector4.zero, new Vector4(0, 1, 0, 0).WithY(0));
+            Assert.AreEqual(new Vector4(0, 0, 1, 0), new Vector4(0, 0, 1, 0).WithY(0));
+
+            Assert.AreEqual(Vector4.one, Vector4.one.WithY(1));
+            Assert.AreEqual(new Vector4(0, 1, 0, 0), Vector4.zero.WithY(1));
+            Assert.AreEqual(new Vector4(0, 1, 0, 1), new Vector4(0, 0, 0, 1).WithY(1));
+            Assert.AreEqual(new Vector4(1, 1, 0, 0), new Vector4(1, 0, 0, 0).WithY(1));
+            Assert.AreEqual(new Vector4(0, 1, 0, 0), new Vector4(0, 1, 0, 0).WithY(1));
+            Assert.AreEqual(new Vector4(0, 1, 1, 0), new Vector4(0, 0, 1, 0).WithY(1));
+            
+            yield break;
+        }
+
+        [UnityTest]
+        public IEnumerator WithZ_ShouldReturnVectorWithNewZ_GivenVector()
+        {
+            Assert.AreEqual(new Vector4(1, 1, 0, 1), Vector4.one.WithZ(0));
+            Assert.AreEqual(Vector4.zero, Vector4.zero.WithZ(0));
+            Assert.AreEqual(new Vector4(0, 0, 0, 1), new Vector4(0, 0, 0, 1).WithZ(0));
+            Assert.AreEqual(new Vector4(1, 0, 0, 0), new Vector4(1, 0, 0, 0).WithZ(0));
+            Assert.AreEqual(new Vector4(0, 1, 0, 0), new Vector4(0, 1, 0, 0).WithZ(0));
+            Assert.AreEqual(Vector4.zero, new Vector4(0, 0, 1, 0).WithZ(0));
+
+            Assert.AreEqual(Vector4.one, Vector4.one.WithZ(1));
+            Assert.AreEqual(new Vector4(0, 0, 1, 0), Vector4.zero.WithZ(1));
+            Assert.AreEqual(new Vector4(0, 0, 1, 1), new Vector4(0, 0, 0, 1).WithZ(1));
+            Assert.AreEqual(new Vector4(1, 0, 1, 0), new Vector4(1, 0, 0, 0).WithZ(1));
+            Assert.AreEqual(new Vector4(0, 1, 1, 0), new Vector4(0, 1, 0, 0).WithZ(1));
+            Assert.AreEqual(new Vector4(0, 0, 1, 0), new Vector4(0, 0, 1, 0).WithZ(1));
+            
+            yield break;
+        }
+    }
+}
---
+++ X10D.Unity/src/Numerics/Vector3Extensions.cs
@@ -0,0 +1,59 @@
+﻿using System.Diagnostics.Contracts;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+namespace X10D.Unity.Numerics;
+
+/// <summary>
+///     Numeric-extensions for <see cref="Vector3" />.
+/// </summary>
+public static class Vector3Extensions
+{
+    /// <summary>
+    ///     Returns a vector whose Y and Z components are the same as the specified vector, and whose X component is a new value.
+    /// </summary>
+    /// <param name="vector">The vector to copy.</param>
+    /// <param name="x">The new X component value.</param>
+    /// <returns>
+    ///     A new instance of <see cref="Vector3" /> whose <see cref="Vector3.y" /> and <see cref="Vector3.z" /> components are
+    ///     the same as that of <paramref name="vector" />, and whose <see cref="Vector3.x" /> component is <paramref name="x" />.
+    /// </returns>
+    [Pure]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Vector3 WithX(this Vector3 vector, float x)
+    {
+        return vector with {x = x};
+    }
+
+    /// <summary>
+    ///     Returns a vector whose X and Z components are the same as the specified vector, and whose Y component is a new value.
+    /// </summary>
+    /// <param name="vector">The vector to copy.</param>
+    /// <param name="y">The new Y component value.</param>
+    /// <returns>
+    ///     A new instance of <see cref="Vector3" /> whose <see cref="Vector3.x" /> and <see cref="Vector3.z" /> components are
+    ///     the same as that of <paramref name="vector" />, and whose <see cref="Vector3.y" /> component is <paramref name="y" />.
+    /// </returns>
+    [Pure]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Vector3 WithY(this Vector3 vector, float y)
+    {
+        return vector with {y = y};
+    }
+
+    /// <summary>
+    ///     Returns a vector whose X and Y components are the same as the specified vector, and whose Z component is a new value.
+    /// </summary>
+    /// <param name="vector">The vector to copy.</param>
+    /// <param name="z">The new Z component value.</param>
+    /// <returns>
+    ///     A new instance of <see cref="Vector3" /> whose <see cref="Vector3.x" /> and <see cref="Vector3.y" /> components are
+    ///     the same as that of <paramref name="vector" />, and whose <see cref="Vector3.z" /> component is <paramref name="z" />.
+    /// </returns>
+    [Pure]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Vector3 WithZ(this Vector3 vector, float z)
+    {
+        return vector with {z = z};
+    }
+}
