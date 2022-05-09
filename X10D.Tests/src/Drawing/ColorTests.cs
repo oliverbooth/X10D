f5b330dffea7cab6047c9780441c456bd4163977
Oliver Booth <me@olivr.me>
2022-05-09 09:51:37 +0100

    [ci skip] Define colors as fields

--- X10D.Tests/src/Drawing/ColorTests.cs
+++ X10D.Tests/src/Drawing/ColorTests.cs
@@ -1,4 +1,4 @@
-﻿using System.Drawing;
+using System.Drawing;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using X10D.Drawing;
 
@@ -7,26 +7,26 @@ namespace X10D.Tests.Drawing;
 [TestClass]
 public class ColorTests
 {
+    private static readonly Color Black = Color.FromArgb(0, 0, 0);
+    private static readonly Color White = Color.FromArgb(255, 255, 255);
+    private static readonly Color Red = Color.FromArgb(255, 0, 0);
+    private static readonly Color Green = Color.FromArgb(0, 255, 0);
+    private static readonly Color Blue = Color.FromArgb(0, 0, 255);
+    private static readonly Color Cyan = Color.FromArgb(0, 255, 255);
+    private static readonly Color Magenta = Color.FromArgb(255, 0, 255);
+    private static readonly Color Yellow = Color.FromArgb(255, 255, 0);
+
     [TestMethod]
     public void Inverted_ShouldReturnInvertedColor()
     {
-        Color black = Color.FromArgb(0, 0, 0);
-        Color white = Color.FromArgb(255, 255, 255);
-        Color red = Color.FromArgb(255, 0, 0);
-        Color green = Color.FromArgb(0, 255, 0);
-        Color blue = Color.FromArgb(0, 0, 255);
-        Color cyan = Color.FromArgb(0, 255, 255);
-        Color magenta = Color.FromArgb(255, 0, 255);
-        Color yellow = Color.FromArgb(255, 255, 0);
-
-        Assert.AreEqual(white, black.Inverted());
-        Assert.AreEqual(black, white.Inverted());
-        Assert.AreEqual(red, cyan.Inverted());
-        Assert.AreEqual(cyan, red.Inverted());
-        Assert.AreEqual(green, magenta.Inverted());
-        Assert.AreEqual(magenta, green.Inverted());
-        Assert.AreEqual(yellow, blue.Inverted());
-        Assert.AreEqual(blue, yellow.Inverted());
+        Assert.AreEqual(White, Black.Inverted());
+        Assert.AreEqual(Black, White.Inverted());
+        Assert.AreEqual(Red, Cyan.Inverted());
+        Assert.AreEqual(Cyan, Red.Inverted());
+        Assert.AreEqual(Green, Magenta.Inverted());
+        Assert.AreEqual(Magenta, Green.Inverted());
+        Assert.AreEqual(Yellow, Blue.Inverted());
+        Assert.AreEqual(Blue, Yellow.Inverted());
     }
 
     [TestMethod]
