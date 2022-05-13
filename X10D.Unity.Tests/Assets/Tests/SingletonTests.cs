0b25f90d0dfd6c6e0303f8d35524bb7c6d9273a9
Oliver Booth <me@olivr.me>
2022-05-13 08:54:00 +0100

    Add Singleton<T> class

---
+++ X10D.Unity.Tests/Assets/Tests/SingletonTests.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace X10D.Unity.Tests
+{
+    public class SingletonTests
+    {
+        [UnityTest]
+        public IEnumerator Singleton_ShouldReturnNewInstance_WhenNoInstanceExists()
+        {
+            TestBehaviour instance = Singleton<TestBehaviour>.Instance;
+            Assert.IsTrue(instance);
+            Assert.IsTrue(instance.Flag);
+
+            yield break;
+        }
+
+        [UnityTest]
+        public IEnumerator Singleton_ShouldReturnSameInstance_WhenAccessedTwice()
+        {
+            TestBehaviour instance = Singleton<TestBehaviour>.Instance;
+            Assert.IsTrue(instance);
+            Assert.AreEqual(instance, Singleton<TestBehaviour>.Instance);
+
+            yield break;
+        }
+
+        [UnityTest]
+        public IEnumerator Singleton_ShouldReturnNewInstance_WhenDestroyed()
+        {
+            TestBehaviour instance = Singleton<TestBehaviour>.Instance;
+            Assert.IsTrue(instance);
+            Object.Destroy(instance);
+            yield return null;
+            Assert.IsFalse(instance);
+
+            // ReSharper disable once HeuristicUnreachableCode
+            instance = Singleton<TestBehaviour>.Instance;
+            Assert.IsNotNull(instance);
+            Assert.IsTrue(instance.Flag);
+        }
+    }
+}
