ec362dc2c0592588ec62d3c622ad66f4fd72a2bc
Oliver Booth <me@olivr.me>
2022-05-23 10:33:52 +0100

    Add MathUtility.InverseLerp (#60)

--- X10D/src/Math/MathUtility.cs
+++ X10D/src/Math/MathUtility.cs
@@ -9,6 +9,54 @@ namespace X10D.Math;
 public static class MathUtility
 {
     /// <summary>
+    ///     Returns the linear interpolation inverse of a value, such that it determines where a value lies between two other
+    ///     values.
+    /// </summary>
+    /// <param name="alpha">The value whose lerp inverse is to be found.</param>
+    /// <param name="start">The start of the range.</param>
+    /// <param name="end">The end of the range.</param>
+    /// <returns>A value determined by <c>(alpha - start) / (end - start)</c>.</returns>
+    [Pure]
+#if NETSTANDARD2_1
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+#else
+    [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
+#endif
+    public static float InverseLerp(float alpha, float start, float end)
+    {
+        if (MathF.Abs(start - end) < float.Epsilon)
+        {
+            return 0f;
+        }
+
+        return (alpha - start) / (end - start);
+    }
+
+    /// <summary>
+    ///     Returns the linear interpolation inverse of a value, such that it determines where a value lies between two other
+    ///     values.
+    /// </summary>
+    /// <param name="alpha">The value whose lerp inverse is to be found.</param>
+    /// <param name="start">The start of the range.</param>
+    /// <param name="end">The end of the range.</param>
+    /// <returns>A value determined by <c>(alpha - start) / (end - start)</c>.</returns>
+    [Pure]
+#if NETSTANDARD2_1
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+#else
+    [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
+#endif
+    public static double InverseLerp(double alpha, double start, double end)
+    {
+        if (System.Math.Abs(start - end) < double.Epsilon)
+        {
+            return 0.0;
+        }
+
+        return (alpha - start) / (end - start);
+    }
+
+    /// <summary>
     ///     Linearly interpolates from one value to a target using a specified alpha.
     /// </summary>
     /// <param name="value">The interpolation source.</param>
