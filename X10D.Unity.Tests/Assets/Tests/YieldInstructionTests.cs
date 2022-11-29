af622a8ef740ac178526b29248a3c3d1d5a75c43
Oliver Booth <me@olivr.me>
2022-11-29 17:45:27 +0000

    Allow for minor differentiation in equality test

--- X10D.Unity.Tests/Assets/Tests/YieldInstructionTests.cs
+++ X10D.Unity.Tests/Assets/Tests/YieldInstructionTests.cs
@@ -14,39 +14,39 @@ namespace X10D.Unity.Tests
         {
             int frameCount = UTime.frameCount;
             yield return new WaitForFrames(10);
-            Assert.AreEqual(frameCount + 10, UTime.frameCount);
+            Assert.AreEqual(frameCount + 10, UTime.frameCount, $"{frameCount + 10} == {UTime.frameCount}");
         }
 
         [UnityTest]
         public IEnumerator WaitForSecondsNoAlloc_ShouldYieldForCorrectTime()
         {
-            var time = (int)UTime.time;
+            float time = UTime.time;
             yield return new WaitForSecondsNoAlloc(2);
-            Assert.AreEqual(time + 2, (int)UTime.time);
+            Assert.AreEqual(time + 2, UTime.time, 1e-2, $"{time + 2} == {UTime.time}");
         }
 
         [UnityTest]
         public IEnumerator WaitForSecondsRealtimeNoAlloc_ShouldYieldForCorrectTime()
         {
-            var time = (int)UTime.time;
+            float time = UTime.time;
             yield return new WaitForSecondsRealtimeNoAlloc(2);
-            Assert.AreEqual(time + 2, (int)UTime.time);
+            Assert.AreEqual(time + 2, UTime.time, 1e-2, $"{time + 2} == {UTime.time}");
         }
 
         [UnityTest]
         public IEnumerator WaitForTimeSpan_ShouldYieldForCorrectTime()
         {
-            var time = (int)UTime.time;
+            float time = UTime.time;
             yield return new WaitForTimeSpan(TimeSpan.FromSeconds(2));
-            Assert.AreEqual(time + 2, (int)UTime.time);
+            Assert.AreEqual(time + 2, UTime.time, 1e-2, $"{time + 2} == {UTime.time}");
         }
 
         [UnityTest]
         public IEnumerator WaitForTimeSpanRealtime_ShouldYieldForCorrectTime()
         {
-            var time = (int)UTime.time;
+            float time = UTime.time;
             yield return new WaitForTimeSpanRealtime(TimeSpan.FromSeconds(2));
-            Assert.AreEqual(time + 2, (int)UTime.time);
+            Assert.AreEqual(time + 2, UTime.time, 1e-2, $"{time + 2} == {UTime.time}");
         }
     }
 }
