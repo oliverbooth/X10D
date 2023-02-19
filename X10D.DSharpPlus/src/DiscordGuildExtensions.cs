5e69f9ca25cd72b44ad73505e306a2d25c2edc35
Oliver Booth <me@olivr.me>
2023-02-19 15:27:51 +0000

    Add DiscordGuild.GetMemberOrNullAsync
    D#+ throws an undocumented NotFoundException when a member does not exist. This method returns null instead

--- X10D.DSharpPlus/src/DiscordGuildExtensions.cs
+++ X10D.DSharpPlus/src/DiscordGuildExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using DSharpPlus;
 using DSharpPlus.Entities;
+using DSharpPlus.Exceptions;
 
 namespace X10D.DSharpPlus;
 
@@ -25,6 +26,39 @@ public static class DiscordGuildExtensions
 #endif
 
         await Task.WhenAll(guild.Threads.Values.Select(t => t.JoinThreadAsync())).ConfigureAwait(false);
+    }
+
+    /// <summary>
+    ///     Gets a guild member by their ID. If the member is not found, <see langword="null" /> is returned instead of
+    ///     <see cref="NotFoundException" /> being thrown.
+    /// </summary>
+    /// <param name="guild">The guild whose member list to search.</param>
+    /// <param name="userId">The ID of the member to retrieve.</param>
+    /// <exception cref="ArgumentNullException"><paramref name="guild" /> is <see langword="null" />.</exception>
+    public static async Task<DiscordMember?> GetMemberOrNullAsync(this DiscordGuild guild, ulong userId)
+    {
+#if NET6_0_OR_GREATER
+        ArgumentNullException.ThrowIfNull(guild);
+#else
+        if (guild is null)
+        {
+            throw new ArgumentNullException(nameof(guild));
+        }
+#endif
+
+        try
+        {
+            // we should never use exceptions for flow control but this is D#+ we're talking about.
+            // NotFoundException isn't even documented, and yet it gets thrown when a member doesn't exist.
+            // so this method should hopefully clearly express that - and at least using exceptions for flow control *here*,
+            // removes the need to do the same in consumer code.
+            // god I hate this.
+            return await guild.GetMemberAsync(userId).ConfigureAwait(false);
+        }
+        catch (NotFoundException)
+        {
+            return null;
+        }
     }
 
     /// <summary>
