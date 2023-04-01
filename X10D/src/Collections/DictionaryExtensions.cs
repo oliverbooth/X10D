oliverbooth/X10D

commit d29663f081f9545210aa9679b462836e4fae4745

style: cleanup DictionaryExtensions
* Explicit type is used where type is not evident.
* Conditions are inlined with a ternary and the return value is used directly.
* System.Runtime.InteropServices is only imported for .NET >= 6.0

X10D/src/Collections/DictionaryExtensions.cs
@@ -1,5 +1,7 @@
 ﻿using System.Diagnostics.Contracts;
+#if NET6_0_OR_GREATER
 using System.Runtime.InteropServices;
+#endif
 using System.Web;
 
 namespace X10D.Collections;
@@ -65,7 +67,7 @@ public static class DictionaryExtensions
 #else
         if (dictionary.TryGetValue(key, out TValue? old))
         {
-            var updated = updateValueFactory(key, old);
+            TValue updated = updateValueFactory(key, old);
             dictionary[key] = updated;
 
             return updated;
@@ -119,7 +121,7 @@ public static class DictionaryExtensions
 
         if (dictionary.TryGetValue(key, out TValue? old))
         {
-            var updated = updateValueFactory(key, old);
+            TValue updated = updateValueFactory(key, old);
             dictionary[key] = updated;
 
             return updated;
@@ -180,26 +182,17 @@ public static class DictionaryExtensions
 
 #if NET6_0_OR_GREATER
         ref TValue? value = ref CollectionsMarshal.GetValueRefOrAddDefault(dictionary, key, out bool exists);
-        if (exists)
-        {
-            value = updateValueFactory(key, value!);
-        }
-        else
-        {
-            value = addValueFactory(key);
-        }
-
-        return value;
+        return exists ? updateValueFactory(key, value!) : addValueFactory(key);
 #else
         if (dictionary.TryGetValue(key, out TValue? old))
         {
-            var updated = updateValueFactory(key, old);
+            TValue updated = updateValueFactory(key, old);
             dictionary[key] = updated;
 
             return updated;
         }
 
-        var add = addValueFactory(key);
+        TValue add = addValueFactory(key);
         dictionary.Add(key, add);
 
         return add;
@@ -257,13 +250,13 @@ public static class DictionaryExtensions
 
         if (dictionary.TryGetValue(key, out TValue? old))
         {
-            var updated = updateValueFactory(key, old);
+            TValue updated = updateValueFactory(key, old);
             dictionary[key] = updated;
 
             return updated;
         }
 
-        var add = addValueFactory(key);
+        TValue add = addValueFactory(key);
         dictionary.Add(key, add);
 
         return add;
@@ -326,26 +319,17 @@ public static class DictionaryExtensions
 
 #if NET6_0_OR_GREATER
         ref TValue? value = ref CollectionsMarshal.GetValueRefOrAddDefault(dictionary, key, out bool exists);
-        if (exists)
-        {
-            value = updateValueFactory(key, value!, factoryArgument);
-        }
-        else
-        {
-            value = addValueFactory(key, factoryArgument);
-        }
-
-        return value;
+        return exists ? updateValueFactory(key, value!, factoryArgument) : addValueFactory(key, factoryArgument);
 #else
         if (dictionary.TryGetValue(key, out TValue? old))
         {
-            var updated = updateValueFactory(key, old, factoryArgument);
+            TValue updated = updateValueFactory(key, old, factoryArgument);
             dictionary[key] = updated;
 
             return updated;
         }
 
-        var add = addValueFactory(key, factoryArgument);
+        TValue add = addValueFactory(key, factoryArgument);
         dictionary.Add(key, add);
 
         return add;
@@ -409,13 +393,13 @@ public static class DictionaryExtensions
 
         if (dictionary.TryGetValue(key, out TValue? old))
         {
-            var updated = updateValueFactory(key, old, factoryArgument);
+            TValue updated = updateValueFactory(key, old, factoryArgument);
             dictionary[key] = updated;
 
             return updated;
         }
 
-        var add = addValueFactory(key, factoryArgument);
+        TValue add = addValueFactory(key, factoryArgument);
         dictionary.Add(key, add);
 
         return add;

commit 34c49a22283fdb9dbbaef840d0b57a180723358c

fix: fix bug introduced by d29663f081f9545210aa9679b462836e4fae4745
Since value is a ref returned value, mutating it before returning was actually intended behaviour, since the reassignment causes the dictionary value to be mutated too.

Also, what game was this ref watching?!

X10D/src/Collections/DictionaryExtensions.cs
@@ -54,16 +54,7 @@ public static class DictionaryExtensions
 
 #if NET6_0_OR_GREATER
         ref var value = ref CollectionsMarshal.GetValueRefOrAddDefault(dictionary, key, out bool exists);
-        if (exists)
-        {
-            value = updateValueFactory(key, value!);
-        }
-        else
-        {
-            value = addValue;
-        }
-
-        return value;
+        return value = exists ? updateValueFactory(key, value!) : addValue;
 #else
         if (dictionary.TryGetValue(key, out TValue? old))
         {
@@ -182,7 +173,7 @@ public static class DictionaryExtensions
 
 #if NET6_0_OR_GREATER
         ref TValue? value = ref CollectionsMarshal.GetValueRefOrAddDefault(dictionary, key, out bool exists);
-        return exists ? updateValueFactory(key, value!) : addValueFactory(key);
+        return value = exists ? updateValueFactory(key, value!) : addValueFactory(key);
 #else
         if (dictionary.TryGetValue(key, out TValue? old))
         {
@@ -319,7 +310,7 @@ public static class DictionaryExtensions
 
 #if NET6_0_OR_GREATER
         ref TValue? value = ref CollectionsMarshal.GetValueRefOrAddDefault(dictionary, key, out bool exists);
-        return exists ? updateValueFactory(key, value!, factoryArgument) : addValueFactory(key, factoryArgument);
+        return value = exists ? updateValueFactory(key, value!, factoryArgument) : addValueFactory(key, factoryArgument);
 #else
         if (dictionary.TryGetValue(key, out TValue? old))
         {