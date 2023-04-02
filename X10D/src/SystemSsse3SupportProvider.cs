92b3e006752579f02bd879ea4a47ac0517a17add
Oliver Booth <me@olivr.me>
2023-04-02 03:44:35 +0100

    ci: exclude cpu-arch support providers from coverage

--- X10D/src/SystemSsse3SupportProvider.cs
+++ X10D/src/SystemSsse3SupportProvider.cs
@@ -1,9 +1,11 @@
-﻿#if NETCOREAPP3_0_OR_GREATER
+﻿using System.Diagnostics.CodeAnalysis;
+#if NETCOREAPP3_0_OR_GREATER
 using System.Runtime.Intrinsics.X86;
 #endif
 
 namespace X10D;
 
+[ExcludeFromCodeCoverage]
 internal struct SystemSsse3SupportProvider : ISsse3SupportProvider
 {
     /// <inheritdoc />
