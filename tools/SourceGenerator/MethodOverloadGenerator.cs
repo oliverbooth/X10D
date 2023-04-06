7556efdfdd286f385d7a12f03bdf18eef01bad41
Oliver Booth <me@olivr.me>
2023-04-06 17:00:39 +0100

    chore: add AutoOverload and OverloadType attribute

---
+++ tools/SourceGenerator/MethodOverloadGenerator.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace SourceGenerator;
+
+[Generator]
+internal sealed class MethodOverloadGenerator : ISourceGenerator
+{
+    /// <inheritdoc />
+    public void Initialize(GeneratorInitializationContext context)
+    {
+        context.RegisterForSyntaxNotifications(() => new OverloadSyntaxReceiver());
+    }
+
+    /// <inheritdoc />
+    public void Execute(GeneratorExecutionContext context)
+    {
+        var syntaxReceiver = (OverloadSyntaxReceiver)context.SyntaxReceiver!;
+        IReadOnlyList<MethodDeclarationSyntax> candidateMethods = syntaxReceiver.CandidateMethods;
+        // TODO implement
+    }
+}
---
+++ tools/SourceGenerator/OverloadSyntaxReceiver.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using X10D.MetaServices;
+
+namespace SourceGenerator;
+
+public class OverloadSyntaxReceiver : ISyntaxReceiver
+{
+    private readonly List<MethodDeclarationSyntax> _candidateMethods = new();
+
+    public IReadOnlyList<MethodDeclarationSyntax> CandidateMethods
+    {
+        get => _candidateMethods.AsReadOnly();
+    }
+
+    public void OnVisitSyntaxNode(SyntaxNode syntaxNode)
+    {
+        if (syntaxNode is not MethodDeclarationSyntax methodDeclarationSyntax)
+        {
+            return;
+        }
+
+        if (methodDeclarationSyntax.AttributeLists.Count == 0)
+        {
+            return;
+        }
+
+        string attributeName = nameof(AutoOverloadAttribute).Replace("Attribute", string.Empty);
+        foreach (AttributeListSyntax attributeListSyntax in methodDeclarationSyntax.AttributeLists)
+        {
+            foreach (AttributeSyntax attributeSyntax in attributeListSyntax.Attributes)
+            {
+                if (attributeSyntax.Name.ToString() == attributeName)
+                {
+                    _candidateMethods.Add(methodDeclarationSyntax);
+                    break;
+                }
+            }
+        }
+    }
+}
