5f7e335b85f74c217a247c2a6253c2239ed8e81c
Oliver Booth <me@olivr.me>
2023-04-11 17:09:14 +0100

    fix: validate null arguments in X10D.Unity
    The CA runs are really testing me today.

--- X10D.Unity/src/GameObjectExtensions.cs
+++ X10D.Unity/src/GameObjectExtensions.cs
@@ -13,8 +13,14 @@ public static class GameObjectExtensions
     /// <param name="gameObject">The game object whose child components to retrieve.</param>
     /// <typeparam name="T">The type of the components to retrieve.</typeparam>
     /// <returns>An array <typeparamref name="T" /> representing the child components.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="gameObject" /> is <see langword="null" />.</exception>
     public static T[] GetComponentsInChildrenOnly<T>(this GameObject gameObject)
     {
+        if (gameObject == null)
+        {
+            throw new ArgumentNullException(nameof(gameObject));
+        }
+
         Transform rootTransform = gameObject.transform;
         var components = new List<T>(gameObject.GetComponentsInChildren<T>());
 
