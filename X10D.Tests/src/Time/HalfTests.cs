e8f75596814bf6f1edd1533b1196a4a525bc73cd
redageddon <therealrubiksmaster@gmail.com>
2023-04-13 13:54:26 -0700

    Fixed the fact that Half tests werent actually testing Half

--- X10D.Tests/src/Time/HalfTests.cs
+++ X10D.Tests/src/Time/HalfTests.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿#if NET5_0_OR_GREATER
+using NUnit.Framework;
 using X10D.Time;
 
 namespace X10D.Tests.Time;
@@ -9,33 +10,34 @@ public class HalfTests
     [Test]
     public void TicksMillisecondsSecondsMinutesDaysHoursWeeks_ShouldBeZero_GivenZero()
     {
-        Assert.That(0.0.Milliseconds(), Is.EqualTo(TimeSpan.Zero));
-        Assert.That(0.0.Seconds(), Is.EqualTo(TimeSpan.Zero));
-        Assert.That(0.0.Minutes(), Is.EqualTo(TimeSpan.Zero));
-        Assert.That(0.0.Days(), Is.EqualTo(TimeSpan.Zero));
-        Assert.That(0.0.Hours(), Is.EqualTo(TimeSpan.Zero));
-        Assert.That(0.0.Weeks(), Is.EqualTo(TimeSpan.Zero));
+        Assert.That(((Half)0.0).Milliseconds(), Is.EqualTo(TimeSpan.Zero));
+        Assert.That(((Half)0.0).Seconds(), Is.EqualTo(TimeSpan.Zero));
+        Assert.That(((Half)0.0).Minutes(), Is.EqualTo(TimeSpan.Zero));
+        Assert.That(((Half)0.0).Days(), Is.EqualTo(TimeSpan.Zero));
+        Assert.That(((Half)0.0).Hours(), Is.EqualTo(TimeSpan.Zero));
+        Assert.That(((Half)0.0).Weeks(), Is.EqualTo(TimeSpan.Zero));
     }
 
     [Test]
     public void TicksMillisecondsSecondsMinutesDaysHoursWeeks_ShouldBePositive_GivenOne()
     {
-        Assert.That(1.0.Milliseconds() > TimeSpan.Zero);
-        Assert.That(1.0.Seconds() > TimeSpan.Zero);
-        Assert.That(1.0.Minutes() > TimeSpan.Zero);
-        Assert.That(1.0.Days() > TimeSpan.Zero);
-        Assert.That(1.0.Hours() > TimeSpan.Zero);
-        Assert.That(1.0.Weeks() > TimeSpan.Zero);
+        Assert.That(((Half)1.0).Milliseconds() > TimeSpan.Zero);
+        Assert.That(((Half)1.0).Seconds() > TimeSpan.Zero);
+        Assert.That(((Half)1.0).Minutes() > TimeSpan.Zero);
+        Assert.That(((Half)1.0).Days() > TimeSpan.Zero);
+        Assert.That(((Half)1.0).Hours() > TimeSpan.Zero);
+        Assert.That(((Half)1.0).Weeks() > TimeSpan.Zero);
     }
 
     [Test]
     public void TicksMillisecondsSecondsMinutesDaysHoursWeeks_ShouldBeNegative_GivenMinusOne()
     {
-        Assert.That((-1.0).Milliseconds() < TimeSpan.Zero);
-        Assert.That((-1.0).Seconds() < TimeSpan.Zero);
-        Assert.That((-1.0).Minutes() < TimeSpan.Zero);
-        Assert.That((-1.0).Days() < TimeSpan.Zero);
-        Assert.That((-1.0).Hours() < TimeSpan.Zero);
-        Assert.That((-1.0).Weeks() < TimeSpan.Zero);
+        Assert.That(((Half)(-1.0)).Milliseconds() < TimeSpan.Zero);
+        Assert.That(((Half)(-1.0)).Seconds() < TimeSpan.Zero);
+        Assert.That(((Half)(-1.0)).Minutes() < TimeSpan.Zero);
+        Assert.That(((Half)(-1.0)).Days() < TimeSpan.Zero);
+        Assert.That(((Half)(-1.0)).Hours() < TimeSpan.Zero);
+        Assert.That(((Half)(-1.0)).Weeks() < TimeSpan.Zero);
     }
 }
+#endif
