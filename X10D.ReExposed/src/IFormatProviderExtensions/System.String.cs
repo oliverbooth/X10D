7048da4f6637bfcd0f1ed6394c67162a4bea7b8d
redageddon <therealrubiksmaster@gmail.com>
2023-04-13 16:22:39 -0700

    updated formatprovider extension naming, ordering and overloading

--- X10D.ReExposed/src/IFormatProviderExtensions/System.String.cs
+++ X10D.ReExposed/src/IFormatProviderExtensions/System.String.cs
@@ -8,49 +8,65 @@
 public static class FormatProviderExtensions
 {
     /// <inheritdoc cref="string.Format(IFormatProvider,string,object)"/>
-    public static string Format(this IFormatProvider formatProvider, string format, object value)
+    public static string Format(this IFormatProvider formatProvider,
+                                [StringSyntax(StringSyntaxAttribute.CompositeFormat)] string format,
+                                object value)
     {
         return string.Format(formatProvider, format, value);
     }
 
     /// <inheritdoc cref="string.Format(IFormatProvider,string,object,object)"/>
-    public static string Format(this IFormatProvider formatProvider, string format, object value, object value2)
+    public static string Format(this IFormatProvider formatProvider,
+                                [StringSyntax(StringSyntaxAttribute.CompositeFormat)] string format,
+                                object value,
+                                object value2)
     {
         return string.Format(formatProvider, format, value, value2);
     }
 
     /// <inheritdoc cref="string.Format(IFormatProvider,string,object,object,object)"/>
-    public static string Format(this IFormatProvider formatProvider, string format, object value, object value2, object value3)
+    public static string Format(this IFormatProvider formatProvider,
+                                [StringSyntax(StringSyntaxAttribute.CompositeFormat)] string format,
+                                object value,
+                                object value2,
+                                object value3)
     {
         return string.Format(formatProvider, format, value, value2, value3);
     }
 
     /// <inheritdoc cref="string.Format(IFormatProvider,string,object[])"/>
-    public static string Format(this IFormatProvider formatProvider, string format, params object[] values)
+    public static string Format(this IFormatProvider formatProvider,
+                                [StringSyntax(StringSyntaxAttribute.CompositeFormat)] string format,
+                                params object[] values)
     {
         return string.Format(formatProvider, format, values);
     }
 
     /// <inheritdoc cref="string.Format(string,object)"/>
-    public static string Format(this string format, object value)
+    public static string Format([StringSyntax(StringSyntaxAttribute.CompositeFormat)] this string format, object value)
     {
         return string.Format(format, value);
     }
 
     /// <inheritdoc cref="string.Format(string,object,object)"/>
-    public static string Format(this string format, object value, object value2)
+    public static string Format([StringSyntax(StringSyntaxAttribute.CompositeFormat)] this string format,
+                                object value,
+                                object value2)
     {
         return string.Format(format, value, value2);
     }
 
     /// <inheritdoc cref="string.Format(string,object,object,object)"/>
-    public static string Format(this string format, object value, object value2, object value3)
+    public static string Format([StringSyntax(StringSyntaxAttribute.CompositeFormat)] this string format,
+                                object value,
+                                object value2,
+                                object value3)
     {
         return string.Format(format, value, value2, value3);
     }
 
     /// <inheritdoc cref="string.Format(string,object[])"/>
-    public static string Format(this string format, params object[] values)
+    public static string Format([StringSyntax(StringSyntaxAttribute.CompositeFormat)] this string format, params object[] values)
     {
         return string.Format(format, values);
     }
