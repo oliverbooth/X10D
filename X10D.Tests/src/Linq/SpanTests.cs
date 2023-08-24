71b0bec85c286bdbee2bd33dac719ba97c1ebe78
Oliver Booth <me@olivr.me>
2023-08-24 01:53:24 +0100

    style(test): format span tests

--- X10D.Tests/src/Linq/SpanTests.cs
+++ X10D.Tests/src/Linq/SpanTests.cs
@@ -1,4 +1,4 @@
-﻿using NUnit.Framework;
+using NUnit.Framework;
 using X10D.Linq;
 
 namespace X10D.Tests.Linq;
@@ -16,7 +16,7 @@ internal class SpanTests
     [Test]
     public void AllShouldBeCorrect()
     {
-        var span = new Span<int>(new[] {2, 4, 6, 8, 10});
+        var span = new Span<int>(new[] { 2, 4, 6, 8, 10 });
         Assert.That(span.All(x => x % 2 == 0));
         Assert.That(span.All(x => x % 2 == 1), Is.False);
     }
@@ -31,7 +31,7 @@ internal class SpanTests
     [Test]
     public void AnyShouldBeCorrect()
     {
-        var span = new Span<int>(new[] {2, 4, 6, 8, 10});
+        var span = new Span<int>(new[] { 2, 4, 6, 8, 10 });
         Assert.That(span.Any(x => x % 2 == 0));
         Assert.That(span.Any(x => x % 2 == 1), Is.False);
     }
@@ -66,7 +66,7 @@ internal class SpanTests
     [Test]
     public void Count_ShouldReturn5_ForEvenNumbers_GivenNumbers1To10()
     {
-        var span = new Span<int>(new[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10});
+        var span = new Span<int>(new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 });
         Assert.That(span.Count(i => i % 2 == 0), Is.EqualTo(5));
     }
 
