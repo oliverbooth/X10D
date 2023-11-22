790b31a635dcf1660bdcbeebfc90e66329db6f21
NN Huy Hoang <destinyhero789@gmail.com>
2023-11-22 22:47:56 +0700

    Remove trailing whitespace

--- X10D/src/CompilerServices/CompilerResources.cs
+++ X10D/src/CompilerServices/CompilerResources.cs
@@ -5,7 +5,7 @@ namespace X10D.CompilerServices;
 internal static class CompilerResources
 {
 #if NETCOREAPP3_0_OR_GREATER
-    public const MethodImplOptions MaxOptimization = System.Runtime.CompilerServices.MethodImplOptions.AggressiveInlining | 
+    public const MethodImplOptions MaxOptimization = System.Runtime.CompilerServices.MethodImplOptions.AggressiveInlining |
                                                      System.Runtime.CompilerServices.MethodImplOptions.AggressiveOptimization;
 #else
     public const MethodImplOptions MaxOptimization = System.Runtime.CompilerServices.MethodImplOptions.AggressiveInlining;
